f96cc3db08f0c74904b10e745ff3ab161266d8a2
Alek Petuskey <alekpetuskey@Aleks-MacBook-Pro.local>
2022-11-19 19:21:09 -0800

    Updated template.

---
+++ example/test/pcconfig.py
@@ -0,0 +1,7 @@
+# The Pynecone configuration file.
+
+APP_NAME = "test"
+API_HOST = "http://localhost:8000"
+BUN_PATH = "$HOME/.bun/bin/bun"
+ENV = "dev"
+DB_URI = "sqlite:///pynecone.db"
---
+++ example/test2/pcconfig.py
@@ -0,0 +1,7 @@
+# The Pynecone configuration file.
+
+APP_NAME = "test2"
+API_HOST = "http://localhost:8000"
+BUN_PATH = "$HOME/.bun/bin/bun"
+ENV = "dev"
+DB_URI = "sqlite:///pynecone.db"
