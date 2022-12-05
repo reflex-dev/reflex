d363d267cecf36863de03823f6706dfbade79d3d
Alek Petuskey <alekpetuskey@Aleks-MacBook-Pro.local>
2022-12-05 02:13:08 -0800

    Added visually hidden component and added iframe/video suppport for box.

--- pynecone/components/layout/box.py
+++ pynecone/components/layout/box.py
@@ -3,15 +3,21 @@
 from pynecone.components.libs.chakra import ChakraComponent
 from pynecone.components.tags import Tag
 from pynecone.var import Var
-
+from typing import Optional
 
 class Box(ChakraComponent):
     """Renders a box component that can contain other components."""
 
     tag = "Box"
 
-    # The element to render.
+    # The type element to render. You can specify as an image, video, or any other HTML element such as iframe.
     element: Var[str]
+
+    # The source of the content.
+    src: Optional[str] = None
+
+    # The alt text of the content.
+    alt: Optional[str] = None
 
     def _render(self) -> Tag:
         return (
