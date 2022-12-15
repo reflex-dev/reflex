747df47ba60fb0e38c2d3c76068520a3b31bf341
Alek Petuskey <alekpetuskey@Aleks-MacBook-Pro.local>
2022-12-14 16:10:05 -0800

    Removed extra imports and hmtl in box.

--- pynecone/components/layout/html.py
+++ pynecone/components/layout/html.py
@@ -1,11 +1,7 @@
 """A html component."""
 
-from typing import Optional
-from pynecone.components.libs.chakra import ChakraComponent
-from pynecone.components.tags import Tag
 from pynecone.components.layout.box import Box
 from pynecone.var import Var
-from pynecone import utils
 from typing import Dict
 
 
