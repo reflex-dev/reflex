6385cffbcd0191671ec83602c0747fb0d2e769ba
Alek Petuskey <alekpetuskey@Aleks-MacBook-Pro.local>
2022-12-22 21:01:18 -0800

    Added cors credentials.

--- pynecone/app.py
+++ pynecone/app.py
@@ -2,7 +2,7 @@
 
 from typing import Any, Callable, Coroutine, Dict, List, Optional, Tuple, Type, Union
 
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket,APIRouter
 from fastapi.middleware import cors
 
 from pynecone import constants, utils
@@ -60,6 +60,7 @@ class App(Base):
         self.state_manager.setup(state=self.state)
 
         # Set up the API.
+        
         self.api = FastAPI()
         self.add_cors()
         self.add_default_endpoints()
@@ -93,6 +94,7 @@ class App(Base):
         self.api.add_middleware(
             cors.CORSMiddleware,
             allow_origins=["*"],
+            allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
         )
