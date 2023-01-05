56c7d4a75026194fe59684f87c63fc2bed4a1859
Alek Petuskey <alekpetuskey@Aleks-MacBook-Pro.local>
2023-01-04 23:03:34 -0800

    Added victory compoenents.

--- pynecone/components/graphing/__init__.py
+++ pynecone/components/graphing/__init__.py
@@ -1,5 +1,5 @@
 """Convenience functions to define layout components."""
 
 from .plotly import Plotly
-
+from .victory import Chart, Line, Scatter, Area, Bar, Pie, Polar, Candlestick, BoxPlot, Histogram, ErrorBar, Group, Stack, Voronoi, 
 __all__ = [f for f in dir() if f[0].isupper()]  # type: ignore
