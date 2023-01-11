92bacab07bf342d28dfae0ebb0bbd3f5ba29b9cd
Mudassir Chapra <37051110+muddi900@users.noreply.github.com>
2023-01-11 08:00:18 +0000

    added new props and event handlers
    fixed name of package
    
    added names to init

--- pynecone/components/datepicker/datepicker.py
+++ pynecone/components/datepicker/datepicker.py
@@ -3,10 +3,17 @@ from pynecone.var import Var
 
 
 class DatePicker(pc.Component):
-    library = "react-date-picker"
+    library = "react-datepicker"
     tag = "DatePicker"
     value: Var[str]
+    date_format: Var[str]
+    close_on_scroll: Var[bool]
+    start_date: Var[str]
+    end_date: Var[str]
+    is_clearable: Var[bool]
+    show_week_numbers: Var[bool]
+    should_close_on_select: Var[bool]
 
     @classmethod
     def get_controlled_triggers(cls) -> set[str]:
-        return {"on_change"}
+        return {"on_change", "on_select"}
