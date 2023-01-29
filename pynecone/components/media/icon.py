3c8864781f6ff19cacd6dbea438c2d5737233504
Lendemor <thomas.brandeho@gmail.com>
2023-01-29 12:12:53 +0100

    catch kw error if tag is missing

--- pynecone/components/media/icon.py
+++ pynecone/components/media/icon.py
@@ -13,3 +13,12 @@ class Icon(ChakraIconComponent):
     """An image icon."""
 
     tag = "None"
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the Icon component.
+
+        Run some additional checks on Icon component.
+        """
+        if "tag" not in kwargs.keys():
+            raise AttributeError("Missing 'tag' keyword-argument for Icon")
+        super().__init__(*args, **kwargs)
