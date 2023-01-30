ef69ca683ea4b09a9797d6425358c9d7d9a16e0f
Alek Petuskey <alekpetuskey@Aleks-MacBook-Pro.local>
2023-01-29 20:08:03 -0800

    darglint

--- pynecone/utils.py
+++ pynecone/utils.py
@@ -630,9 +630,6 @@ def terminate_port(port, _type):
     Args:
         port: The port.
         _type: The type of the port.
-
-    Raises:
-        SystemExit: If the user does not want to kill the process on the port.
     """
     console.print(
         f"Something is already running on port [bold underline]{port}[/bold underline]. This is the port the {_type} runs on."
