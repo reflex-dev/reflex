eb723b6ebe67ac5570043d243d1763cb4148764b
Nikhil Rao <nikhil@pynecone.io>
2023-02-08 16:27:13 -0800

    Add compile error for invalid event handlers (#482)

--- pynecone/components/component.py
+++ pynecone/components/component.py
@@ -165,6 +165,9 @@ class Component(Base, ABC):
         Raises:
             ValueError: If the value is not a valid event chain.
         """
+        # Check if the trigger is a controlled event.
+        is_controlled_event = event_trigger in self.get_controlled_triggers()
+
         # If it's an event chain var, return it.
         if isinstance(value, Var):
             if value.type_ is not EventChain:
@@ -182,8 +185,19 @@ class Component(Base, ABC):
             events = []
             for v in value:
                 if isinstance(v, EventHandler):
-                    events.append(utils.call_event_handler(v, arg))
+                    # Call the event handler to get the event.
+                    event = utils.call_event_handler(v, arg)
+
+                    # Check that the event handler takes no args if it's uncontrolled.
+                    if not is_controlled_event and len(event.args) > 0:
+                        raise ValueError(
+                            f"Event handler: {v.fn} for uncontrolled event {event_trigger} should not take any args."
+                        )
+
+                    # Add the event to the chain.
+                    events.append(event)
                 elif isinstance(v, Callable):
+                    # Call the lambda to get the event chain.
                     events.extend(utils.call_event_fn(v, arg))
                 else:
                     raise ValueError(f"Invalid event: {v}")
@@ -197,7 +211,7 @@ class Component(Base, ABC):
             raise ValueError(f"Invalid event chain: {value}")
 
         # Add args to the event specs if necessary.
-        if event_trigger in self.get_controlled_triggers():
+        if is_controlled_event:
             events = [
                 EventSpec(
                     handler=e.handler,
