64b0d47897191b5b86056fd69e0015b49bf40143
Nikhil Rao <nikhil@pynecone.io>
2023-02-09 21:46:18 -0800

    Hardcode bun version (#494)

--- pynecone/constants.py
+++ pynecone/constants.py
@@ -68,7 +68,7 @@ API_URL = "http://localhost:8000"
 # The default path where bun is installed.
 BUN_PATH = "$HOME/.bun/bin/bun"
 # Command to install bun.
-INSTALL_BUN = "curl https://bun.sh/install | bash"
+INSTALL_BUN = "curl -fsSL https://bun.sh/install | bash -s -- bun-v0.5.5"
 # Default host in dev mode.
 BACKEND_HOST = "0.0.0.0"
 # The default timeout when launching the gunicorn server.
