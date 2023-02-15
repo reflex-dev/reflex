6d97235cbefa7db47e11add2e339c7deaf4d0ed6
yamashita.y <yyamashita1201@gmail.com>
2023-02-15 20:02:48 +0900

    Add windows darglint test batch file

---
+++ darglint_test.bat
@@ -0,0 +1,21 @@
+@echo off
+
+echo "start darglint"
+
+echo "pynecon folder"
+for /R pynecone %%f in (*.py) do (
+    echo %%f
+    echo %%f|findstr /r "^.*pynecone\\pc\.py$"
+    if errorlevel 1 (
+        poetry run darglint %%f
+    )
+)
+
+echo "tests folder"
+for /R tests %%f in (*.py) do (
+    echo %%f
+    poetry run darglint %%f
+)
+
+echo "darglint finished"
+pause
