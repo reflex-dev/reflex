2ee8c73d5a9a7a2b8ff514b4c7eb6debacb546cb
Nikhil Rao <nikhil@pynecone.io>
2023-03-11 21:11:59 -0800

    Add error message for invalid var ops (#664)

--- tests/components/datadisplay/conftest.py
+++ tests/components/datadisplay/conftest.py
@@ -1,3 +1,5 @@
+"""Data display component tests fixtures."""
+
 import pytest
 
 import pynecone as pc
@@ -5,6 +7,15 @@ import pynecone as pc
 
 @pytest.fixture
 def data_table_state(request):
+    """Get a data table state.
+
+    Args:
+        request: The request.
+
+    Returns:
+        The data table state class.
+    """
+
     class DataTableState(pc.State):
         data = request.param["data"]
         columns = ["column1", "column2"]
