c29062e05446663f8f8826622e58c0c668423fc3
Kabir Goel <kabirgoel.kg@gmail.com>
2023-03-15 23:55:26 -0700

    More refactoring

--- pynecone/el/precompile.py
+++ pynecone/el/precompile.py
@@ -12,6 +12,8 @@ running this script.
 
 import os
 
+from pynecone.compiler.templates import join
+
 from .constants import ELEMENT_TO_PROPS, ELEMENTS
 
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -19,35 +21,50 @@ ELEMENTS_DIR = os.path.join(FILE_DIR, "elements")
 INIT_PY_PATH = os.path.join(ELEMENTS_DIR, "__init__.py")
 
 
-def pyclass_name(element: str) -> str:
-    """Get the name of the Python class for the given element."""
-    return element.capitalize()
-
-
 def element_path(element: str) -> str:
     """Get the name of the Python file for the given element."""
-    return os.path.join(ELEMENTS_DIR, f"{element}.py")
+    return join(ELEMENTS_DIR, f"{element}.py")
+
+
+PROP = "    {prop}: PCVar[Union[str, int, bool]]".format
 
 
 def compile_pyclass_props(element: str):
     """Compile props for an element."""
-    return "\n".join(
-        f"    {prop}: PCVar[Union[str, int, bool]]"
-        for prop in ELEMENT_TO_PROPS[element]
+    return join(PROP(prop=prop) for prop in ELEMENT_TO_PROPS[element])
+
+
+PYCLASS = join(
+    [
+        "",
+        "class {name}(Element):  # noqa: E742",
+        '    """Display the {element} element."""',
+        "",
+        '    tag = "{element}"',
+        "",
+        "{props}",
+        "",
+        "{call_name} = {name}.create",
+        "",
+    ]
+).format
+
+
+def compile_pyclass(element: str):
+    """Compile a Python class for an element."""
+    name = element.capitalize()
+    props = compile_pyclass_props(element)
+
+    # Handle the `del` element, which is a Python keyword. Note that the class
+    # name is still `Del`.
+    call_name = "del_" if element == "del" else element
+
+    return PYCLASS(
+        name=name,
+        element=element,
+        props=props,
+        call_name=call_name,
     )
-
-
-TEMPLATE = """
-class {pyclass}(Element):  # noqa: E742
-    \"\"\"Display the {name} element.\"\"\"
-
-    tag = "{name}"
-
-{props}
-
-
-{name} = {pyclass}.create
-"""
 
 
 INIT_PY = [
@@ -61,23 +78,9 @@ INIT_PY = [
 
 
 for element in sorted(ELEMENTS):
-    # Name of the Python class we're generating.
-    pyclass = pyclass_name(element)
-
-    # Handle the "del" element, which is a Python keyword.
-    # Note that the class name is still "Del".
-    element_name_override = "del_" if element == "del" else element
-
-    code = TEMPLATE.format(
-        name=element_name_override,
-        pyclass=pyclass,
-        props=compile_pyclass_props(element),
-    )
-
-    # Add the element to the __init__.py file.
-    INIT_PY.append(code)
+    INIT_PY.append(compile_pyclass(element))
 
 
 os.makedirs(ELEMENTS_DIR, exist_ok=True)
 with open(INIT_PY_PATH, "w+") as f:
-    f.write("\n".join(INIT_PY))
+    f.write(join(INIT_PY))
