2cbedae0bc41767da5333edfd14a2eefc5784315
milochen0418 <milo_chen@mail2000.com.tw>
2023-05-06 20:22:19 +0800

    min node version 16.6.0 for nba example

--- pynecone/constants.py
+++ pynecone/constants.py
@@ -15,7 +15,7 @@ PACKAGE_NAME = "pynecone"
 # The current version of Pynecone.
 VERSION = pkg_resources.get_distribution(PACKAGE_NAME).version
 # Minimum version of Node.js required to run Pynecone.
-MIN_NODE_VERSION = "12.22.0"
+MIN_NODE_VERSION = "16.6.0"
 
 # Valid bun versions.
 MIN_BUN_VERSION = "0.5.8"
