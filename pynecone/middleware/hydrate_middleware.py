733037f473718c6650eb41bec3f7c08c82088efe
Nikhil Rao <nikhil@pynecone.io>
2023-05-08 12:03:07 -0700

    Remove breakpoint

--- pynecone/middleware/hydrate_middleware.py
+++ pynecone/middleware/hydrate_middleware.py
@@ -45,7 +45,6 @@ class HydrateMiddleware(Middleware):
         # Add the on_load events and set is_hydrated to True.
         events = [*app.get_load_events(route), type(state).set_is_hydrated(True)]  # type: ignore
         events = fix_events(events, event.token)
-        # breakpoint()
 
         # Return the state update.
         return StateUpdate(delta=delta, events=events)
