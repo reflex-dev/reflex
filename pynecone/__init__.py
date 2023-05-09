c24dcf4a115787ec527fbff9f6c05683287b1c7e
jh <jason.hottelet@tuta.io>
2023-05-09 21:02:19 +0200

    fix: Reexport modules explicitly
    This fixes a pyright error
    
    #978

--- pynecone/__init__.py
+++ pynecone/__init__.py
@@ -1,29 +1,33 @@
 """Import all classes and functions the end user will need to make an app.
 
 Anything imported here will be available in the default Pynecone import as `pc.*`.
+To signal to typecheckers that something should be reexported, 
+we use the Flask "import name as name" syntax.
 """
 
-from . import el
-from .app import App, UploadFile
-from .base import Base
+from . import el as el
+from .app import App as App
+from .app import UploadFile as UploadFile
+from .base import Base as Base
 from .components import *
 from .components.component import custom_component as memo
-from .components.graphing.victory import data
-from .config import Config, DBConfig
-from .constants import Env, Transports
-from .event import (
-    EVENT_ARG,
-    EventChain,
-    console_log,
-    redirect,
-    set_value,
-    window_alert,
-)
+from .components.graphing.victory import data as data
+from .config import Config as Config
+from .config import DBConfig as DBConfig
+from .constants import Env as Env
+from .constants import Transports as Transports
+from .event import EVENT_ARG as EVENT_ARG
+from .event import EventChain as EventChain
 from .event import FileUpload as upload_files
-from .middleware import Middleware
-from .model import Model, session
-from .route import route
+from .event import console_log as console_log
+from .event import redirect as redirect
+from .event import set_value as set_value
+from .event import window_alert as window_alert
+from .middleware import Middleware as Middleware
+from .model import Model as Model
+from .model import session as session
+from .route import route as route
 from .state import ComputedVar as var
-from .state import State
-from .style import toggle_color_mode
-from .var import Var
+from .state import State as State
+from .style import toggle_color_mode as toggle_color_mode
+from .var import Var as Var
