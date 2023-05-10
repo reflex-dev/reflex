f387dfd20ab779fedaec0f1b4f7ccae393c5bd4d
Unknown6334 <63234449+Unknown1934-del@users.noreply.github.com>
2023-05-10 12:00:36 +0530

    Update switch.py

--- pynecone/components/forms/switch.py
+++ pynecone/components/forms/switch.py
@@ -3,7 +3,7 @@ from typing import Dict
 
 from pynecone.components.component import EVENT_ARG
 from pynecone.components.libs.chakra import ChakraComponent
-from pynecone.vars import Var
+from pynecone.var import Var
 
 
 class Switch(ChakraComponent):
