6c60295ba1ae628f8b2d7cc2def0d8303fdc8047
Unknown6334 <63234449+Unknown1934-del@users.noreply.github.com>
2023-05-12 05:42:50 +0530

    Add color_scheme prop to pc.switch (#990)

--- pynecone/components/forms/switch.py
+++ pynecone/components/forms/switch.py
@@ -38,6 +38,9 @@ class Switch(ChakraComponent):
     # The placeholder text.
     placeholder: Var[str]
 
+    # The color scheme of the switch (e.g. "blue", "green", "red", etc.)
+    color_scheme: Var[str]
+
     @classmethod
     def get_controlled_triggers(cls) -> Dict[str, Var]:
         """Get the event triggers that pass the component's value to the handler.
