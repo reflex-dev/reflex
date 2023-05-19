ad220e346a6d0d37aabd9d4b15e1bda0cc1a6ada
Unknown Sentinel <63234449+Unknownsentinel193@users.noreply.github.com>
2023-05-19 21:06:48 +0530

    Update __init__.py

--- pynecone/components/others/__init__.py
+++ pynecone/components/others/__init__.py
@@ -1,2 +1,9 @@
-""" Other Components """
-from .transitions import Transitions, TransitionsFade, TransitionsScaleFade, TransitionsSlide, TransitionsSlideFade, TransitionsCollapse
+"""Other Components."""
+from .transitions import (
+    Transitions,
+    TransitionsCollapse,
+    TransitionsFade,
+    TransitionsScaleFade,
+    TransitionsSlide,
+    TransitionsSlideFade,
+)
