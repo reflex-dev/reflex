5b14d056b6be6a0dbccdc075405e3d2f5c6760f8
Alek Petuskey <alekpetuskey@Aleks-MacBook-Pro.local>
2023-05-23 16:48:28 -0700

    Remove port test.

--- tests/test_tunnel.py
+++ tests/test_tunnel.py
@@ -10,10 +10,3 @@ from pynecone.utils import tunnel
 def test_remove_file_extensions(input_string, expected_output):
     output = tunnel.remove_file_extensions(input_string)
     assert output == expected_output
-
-
-def test_is_port_in_use_socket_error():
-    ip = "127.0.0.1"
-    port = 8000
-
-    assert tunnel.is_port_in_use(ip, port) == False
