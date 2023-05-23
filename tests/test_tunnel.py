bc8f583058b0d9ee076382c940ec7decfdbfb071
Alek Petuskey <alekpetuskey@Aleks-MacBook-Pro.local>
2023-05-23 16:36:04 -0700

    Get rid of unused ports test

--- tests/test_tunnel.py
+++ tests/test_tunnel.py
@@ -17,16 +17,3 @@ def test_is_port_in_use_socket_error():
     port = 8000
 
     assert tunnel.is_port_in_use(ip, port) is False
-
-
-def test_find_unused_ports():
-    ip_address = "3.101.35.30"
-    start_port = 8000
-    end_port = 8100
-    num_ports = 2
-
-    unused_ports = tunnel.find_unused_ports(ip_address, start_port, end_port, num_ports)
-
-    assert len(unused_ports) == num_ports
-    assert all(port >= start_port and port < end_port for port in unused_ports)
-    assert all(not tunnel.is_port_in_use(ip_address, port) for port in unused_ports)
