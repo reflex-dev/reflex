8ec546352640558694ffb20af2abdcc28a612267
Unknown Sentinel <63234449+Unknownsentinel193@users.noreply.github.com>
2023-05-26 14:39:00 +0530

    Refactor Html component to support HTML tag rendering.

--- pynecone/components/layout/html.py
+++ pynecone/components/layout/html.py
@@ -9,7 +9,7 @@ class Html(Box):
     """Render the html.
 
     Returns:
-        The code to render the  html component.
+        The code to render the html component.
     """
 
     # The HTML to render.
@@ -17,7 +17,7 @@ class Html(Box):
 
     @classmethod
     def create(cls, *children, **props):
-        """Create a html component.
+        """Create an html component.
 
         Args:
             *children: The children of the component.
@@ -31,9 +31,35 @@ class Html(Box):
         """
         # If children are not provided, throw an error.
         if len(children) != 1:
-            raise ValueError("Must provide children to the html component.")
+            raise ValueError("Must provide a single child to the html component.")
         else:
-            props["dangerouslySetInnerHTML"] = {"__html": children[0]}
+            child = children[0]
+            wrapped_child = child  # Initialize with the child as is
+
+            # Check for header tags and wrap them accordingly
+            if child.startswith("<h1>") and child.endswith("</h1>"):
+                wrapped_child = f"<h1>{child[4:-5]}</h1>"
+            elif child.startswith("<h2>") and child.endswith("</h2>"):
+                wrapped_child = f"<h2>{child[4:-5]}</h2>"
+            elif child.startswith("<h3>") and child.endswith("</h3>"):
+                wrapped_child = f"<h3>{child[4:-5]}</h3>"
+            elif child.startswith("<h4>") and child.endswith("</h4>"):
+                wrapped_child = f"<h4>{child[4:-5]}</h4>"
+            elif child.startswith("<h5>") and child.endswith("</h5>"):
+                wrapped_child = f"<h5>{child[4:-5]}</h5>"
+            elif child.startswith("<h6>") and child.endswith("</h6>"):
+                wrapped_child = f"<h6>{child[4:-5]}</h6>"
+            # Add more conditions for other header tags if needed
+
+            # Check for bold tag and wrap accordingly
+            elif child.startswith("<strong>") and child.endswith("</strong>"):
+                wrapped_child = f"<strong>{child[8:-9]}</strong>"
+
+            # Check for italic tag and wrap accordingly
+            elif child.startswith("<em>") and child.endswith("</em>"):
+                wrapped_child = f"<em>{child[4:-5]}</em>"
+
+            props["dangerouslySetInnerHTML"] = {"__html": wrapped_child}
 
         # Create the component.
         return super().create(**props)
