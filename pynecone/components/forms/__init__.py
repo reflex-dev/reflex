1de615c4b68d2c496de90778089e33e4086d8b20
Christopher Terrazas <12790854+cterrazas2@users.noreply.github.com>
2023-05-28 06:11:01 +0000

    Add email component (#1093)

--- pynecone/components/forms/__init__.py
+++ pynecone/components/forms/__init__.py
@@ -4,6 +4,7 @@ from .button import Button, ButtonGroup
 from .checkbox import Checkbox, CheckboxGroup
 from .copytoclipboard import CopyToClipboard
 from .editable import Editable, EditableInput, EditablePreview, EditableTextarea
+from .email import Email
 from .form import Form, FormControl, FormErrorMessage, FormHelperText, FormLabel
 from .iconbutton import IconButton
 from .input import Input, InputGroup, InputLeftAddon, InputRightAddon
