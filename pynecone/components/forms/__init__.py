7fb7ebfd1234348d18cbab80253c213e2be71aca
aidanjrauscher <ajrscomputer@gmail.com>
2023-06-04 21:09:27 -0400

    fixed naming for datepicker and datetimepicker

--- pynecone/components/forms/__init__.py
+++ pynecone/components/forms/__init__.py
@@ -3,8 +3,8 @@
 from .button import Button, ButtonGroup
 from .checkbox import Checkbox, CheckboxGroup
 from .copytoclipboard import CopyToClipboard
-from .datepicker import DatePicker
-from .datetimepicker import DateTimePicker
+from .date_picker import DatePicker
+from .date_time_picker import DateTimePicker
 from .editable import Editable, EditableInput, EditablePreview, EditableTextarea
 from .email import Email
 from .form import Form, FormControl, FormErrorMessage, FormHelperText, FormLabel
