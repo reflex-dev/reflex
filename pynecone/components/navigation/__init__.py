64fc12a5b9e2da5d9e8e9c9e680d1abbba6422de
Aidan Rauscher <ajrscomputer@gmail.com>
2023-06-06 15:05:48 -0400

    Add Chakra Stepper Component (#1142)

--- pynecone/components/navigation/__init__.py
+++ pynecone/components/navigation/__init__.py
@@ -4,5 +4,16 @@ from .breadcrumb import Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSe
 from .link import Link
 from .linkoverlay import LinkBox, LinkOverlay
 from .nextlink import NextLink
+from .stepper import (
+    Step,
+    StepDescription,
+    StepIcon,
+    StepIndicator,
+    StepNumber,
+    Stepper,
+    StepSeparator,
+    StepStatus,
+    StepTitle,
+)
 
 __all__ = [f for f in dir() if f[0].isupper()]  # type: ignore
