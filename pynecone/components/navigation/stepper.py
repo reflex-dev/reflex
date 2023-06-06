83633d0e1d2783871dd2dc39febe0d9a787fed41
aidanjrauscher <ajrscomputer@gmail.com>
2023-06-06 00:57:08 -0400

    changed type annotation

--- pynecone/components/navigation/stepper.py
+++ pynecone/components/navigation/stepper.py
@@ -25,7 +25,7 @@ class Stepper(ChakraComponent):
     size: Var[str]
 
     @classmethod
-    def create(cls, *children, items:Optional[List[Tuple[Var[ChakraComponent],Var[Component],Var[ChakraComponent]]]]=None, **props) -> Component:
+    def create(cls, *children, items:Optional[List[Tuple]]=None, **props) -> Component:
         """Create a Stepper component.
 
         If the kw-args `items` is provided and is a list, they will be added as children.
