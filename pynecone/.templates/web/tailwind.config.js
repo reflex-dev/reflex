9a648e84045d696d16c8ff4a59a13f562a6b484d
Kabir Goel <kabirgoel.kg@gmail.com>
2023-06-06 18:53:49 -0700

    Add prerequisites for first-class Tailwind support

---
+++ pynecone/.templates/web/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
