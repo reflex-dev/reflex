9e8b17f38ac050e2f1dc5fe273da80134a3cacbc
jiangmiemie <69161207+jiangmiemie@users.noreply.github.com>
2023-06-08 11:15:16 +0800

    Update popover.py

--- pynecone/components/overlay/popover.py
+++ pynecone/components/overlay/popover.py
@@ -111,19 +111,19 @@ class Popover(ChakraComponent):
         if len(children) == 0:
             contents = []
 
-            trigger = PopoverTrigger.create(trigger)
+            trigger = PopoverTrigger.create(trigger_comp)
 
             # add header if present in props
-            if header:
-                contents.append(PopoverHeader.create(header))
+            if header_comp:
+                contents.append(PopoverHeader.create(header_comp))
 
-            if body:
-                contents.append(PopoverBody.create(body))
+            if body_comp:
+                contents.append(PopoverBody.create(body_comp))
 
-            if footer:
-                contents.append(PopoverFooter.create(footer))
+            if footer_comp:
+                contents.append(PopoverFooter.create(footer_comp))
 
-            if use_close_button:
+            if use_close_button_comp:
                 contents.append(PopoverCloseButton.create())
 
             children = [trigger, PopoverContent.create(*contents)]
