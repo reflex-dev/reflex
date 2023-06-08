284aeefccc11fca6d355fad1d399a63e68151b64
milochen0418 <milo_chen@mail2000.com.tw>
2023-06-09 01:52:29 +0800

    Create empty directory for pc.get_asset_path() when .web init

--- pynecone/utils/prerequisites.py
+++ pynecone/utils/prerequisites.py
@@ -209,6 +209,7 @@ def initialize_web_directory():
     path_ops.rm(os.path.join(constants.WEB_TEMPLATE_DIR, constants.NODE_MODULES))
     path_ops.rm(os.path.join(constants.WEB_TEMPLATE_DIR, constants.PACKAGE_LOCK))
     path_ops.cp(constants.WEB_TEMPLATE_DIR, constants.WEB_DIR)
+    path_ops.mkdir(constants.WEB_ASSETS_DIR)
 
     # Write the current version of distributed pynecone package to a PCVERSION_APP_FILE."""
     with open(constants.PCVERSION_APP_FILE, "w") as f:
