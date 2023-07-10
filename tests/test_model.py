bb97f019b30c064282a34a6a6f3f95d555e34444
Masen Furer <m_github@0x26.net>
2023-07-10 14:49:19 -0700

    test_model: add integration test for automigration
    add / drop table
    add / drop column
    change column type and default -> should be no-op

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,13 @@
+import subprocess
+import sys
+from unittest import mock
+
 import pytest
+import sqlalchemy
 import sqlmodel
 
+import reflex.constants
+import reflex.model
 from reflex.model import Model
 
 
@@ -49,3 +56,122 @@ def test_custom_primary_key(model_custom_primary):
         model_custom_primary: Fixture.
     """
     assert "id" not in model_custom_primary.__class__.__fields__
+
+
+@pytest.mark.filterwarnings(
+    "ignore:This declarative base already contains a class with the same class name",
+)
+def test_automigration(tmp_working_dir, monkeypatch):
+    """Test alembic automigration with add and drop table and column.
+
+    Args:
+        tmp_working_dir: directory where database and migrations are stored
+        monkeypatch: pytest fixture to overwrite attributes
+    """
+    subprocess.run(
+        [sys.executable, "-m", "alembic", "init", "alembic"],
+        cwd=tmp_working_dir,
+    )
+    alembic_ini = tmp_working_dir / "alembic.ini"
+    versions = tmp_working_dir / "alembic" / "versions"
+    assert alembic_ini.exists()
+    assert versions.exists()
+
+    config_mock = mock.Mock()
+    config_mock.db_url = f"sqlite:///{tmp_working_dir}/reflex.db"
+    monkeypatch.setattr(reflex.model, "get_config", mock.Mock(return_value=config_mock))
+    monkeypatch.setattr(reflex.constants, "ALEMBIC_CONFIG", str(alembic_ini))
+
+    # initial table
+    class AlembicThing(Model, table=True):  # type: ignore
+        t1: str
+
+    Model.automigrate()
+    assert len(list(versions.glob("*.py"))) == 1
+
+    with reflex.model.session() as session:
+        session.add(AlembicThing(id=None, t1="foo"))
+        session.commit()
+
+    sqlmodel.SQLModel.metadata.clear()
+
+    # Create column t2
+    class AlembicThing(Model, table=True):  # type: ignore
+        t1: str
+        t2: str = "bar"
+
+    Model.automigrate()
+    assert len(list(versions.glob("*.py"))) == 2
+
+    with reflex.model.session() as session:
+        result = session.exec(AlembicThing.select).all()
+        assert len(result) == 1
+        assert result[0].t2 == "bar"
+
+    sqlmodel.SQLModel.metadata.clear()
+
+    # Drop column t1
+    class AlembicThing(Model, table=True):  # type: ignore
+        t2: str = "bar"
+
+    Model.automigrate()
+    assert len(list(versions.glob("*.py"))) == 3
+
+    with reflex.model.session() as session:
+        result = session.exec(AlembicThing.select).all()
+        assert len(result) == 1
+        assert result[0].t2 == "bar"
+
+    # Add table
+    class AlembicSecond(Model, table=True):  # type: ignore
+        a: int = 42
+        b: float = 4.2
+
+    Model.automigrate()
+    assert len(list(versions.glob("*.py"))) == 4
+
+    with reflex.model.session() as session:
+        session.add(AlembicSecond(id=None))
+        session.commit()
+        result = session.exec(AlembicSecond.select).all()
+        assert len(result) == 1
+        assert result[0].a == 42
+        assert result[0].b == 4.2
+
+    # No-op
+    Model.automigrate()
+    assert len(list(versions.glob("*.py"))) == 4
+
+    # drop table (AlembicSecond)
+    sqlmodel.SQLModel.metadata.clear()
+
+    class AlembicThing(Model, table=True):  # type: ignore
+        t2: str = "bar"
+
+    Model.automigrate()
+    assert len(list(versions.glob("*.py"))) == 5
+
+    with reflex.model.session() as session:
+        with pytest.raises(sqlalchemy.exc.OperationalError) as errctx:  # type: ignore
+            session.exec(AlembicSecond.select).all()
+        assert errctx.match(r"no such table: alembicsecond")
+        # first table should still exist
+        result = session.exec(AlembicThing.select).all()
+        assert len(result) == 1
+        assert result[0].t2 == "bar"
+
+    sqlmodel.SQLModel.metadata.clear()
+
+    class AlembicThing(Model, table=True):  # type: ignore
+        # changing column type not supported by default
+        t2: int = 42
+
+    Model.automigrate()
+    assert len(list(versions.glob("*.py"))) == 5
+
+    # clear all metadata to avoid influencing subsequent tests
+    sqlmodel.SQLModel.metadata.clear()
+
+    # drop remaining tables
+    Model.automigrate()
+    assert len(list(versions.glob("*.py"))) == 6
