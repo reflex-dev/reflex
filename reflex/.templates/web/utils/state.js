4044cec20246528aeafc6752bba22618c34a09ba
Elijah <elijahahianyo@gmail.com>
2023-07-17 11:09:16 +0000

    handle axios exceptions

--- reflex/.templates/web/utils/state.js
+++ reflex/.templates/web/utils/state.js
@@ -323,8 +323,27 @@ export const uploadFiles = async (state, setResult, handler) => {
   }
 
   // Send the file to the server.
-  await axios.post(UPLOADURL, formdata, headers);
-  return true;
+  await axios.post(UPLOADURL, formdata, headers)
+    .then(() => { return true; })
+    .catch(
+      error => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        return false;
+      }
+    )
+  //  return true;
 };
 
 /**
