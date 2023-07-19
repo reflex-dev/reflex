6978b11977e381821315d6259c32b62df0c31d20
Masen Furer <m_github@0x26.net>
2023-07-19 15:10:31 -0700

    Add ConnectionModal for displaying connection errors
    * expose `connection_error` var for downstream use
    * expose `has_connection_error` var for downstream use
    * expose `default_connection_error` as list[str | Var] for downstream use with
      other components or styles without duplicating the error message
    * include API_URL in the error message so users can easily see what the frontend
      was trying to connect to
    
    Create `rx.connection_modal` which takes over the page and definitely does NOT
    give any illusion as to the page interactivity status.

--- reflex/components/overlay/banner.py
+++ reflex/components/overlay/banner.py
@@ -1,10 +1,30 @@
 """Banner components."""
 from typing import Optional
 
+from reflex import constants
 from reflex.components.component import Component
 from reflex.components.layout import Box, Cond, Fragment
+from reflex.components.overlay.modal import Modal
 from reflex.components.typography import Text
 from reflex.vars import Var
+
+connection_error = Var.create(
+    value="(connectError !== null) ? connectError.message : ''",
+    is_local=False,
+    is_string=False,
+)
+has_connection_error = Var.create(
+    value="connectError !== null",
+    is_string=False,
+)
+if has_connection_error is not None:
+    has_connection_error.type_ = bool
+default_connection_error = [
+    "Cannot connect to server: ",
+    connection_error,
+    ". Check if server is reachable at ",
+    constants.API_URL,
+]
 
 
 class ConnectionBanner(Cond):
@@ -23,11 +43,33 @@ class ConnectionBanner(Cond):
         if not comp:
             comp = Box.create(
                 Text.create(
-                    "cannot connect to server. Check if server is reachable",
+                    *default_connection_error,
                     bg="red",
                     color="white",
                 ),
                 textAlign="center",
             )
 
-        return super().create(Var.create("notConnected"), comp, Fragment.create())  # type: ignore
+        return super().create(has_connection_error, comp, Fragment.create())  # type: ignore
+
+
+class ConnectionModal(Modal):
+    """A connection status modal window."""
+
+    @classmethod
+    def create(cls, comp: Optional[Component] = None) -> Component:
+        """Create a connection banner component.
+
+        Args:
+            comp: The component to render when there's a server connection error.
+
+        Returns:
+            The connection banner component.
+        """
+        if not comp:
+            comp = Text.create(*default_connection_error)
+        return super().create(
+            header="Connection Error",
+            body=comp,
+            is_open=has_connection_error,
+        )
