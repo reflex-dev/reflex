a8ac30a037d250c993b2abd1679552836de71586
Jackie Tung <jackie@pynecone.io>
2023-07-27 10:59:06 -0700

    hiding script

---
+++ scripts/hide_node_and_friends_from_system.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+NODE_AND_FRIENDS=(node npm nvm bun)
+
+for n in "${NODE_AND_FRIENDS[@]}"; do
+  echo "checking for $n"
+  if which -s "$n"; then
+    echo "$n found in system path"
+    for f in $(which -a "$n"); do
+      echo "- Deleting $f"
+      rm -f "$f"
+    done
+  else
+    echo "$n not found (great!)"
+  fi
+done
+
+for n in "${NODE_AND_FRIENDS[@]}"; do
+  if which -s "$n"; then
+    echo "$n STILL found in system path after attempt to delete them"
+    exit 1
+  fi
+done
