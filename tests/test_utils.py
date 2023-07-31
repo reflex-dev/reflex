dc24544bc9b2355a7bfa5556bae14431422411f3
Nikhil Rao <nikhil@pynecone.io>
2023-07-30 20:36:18 -0700

    Fix test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -537,7 +537,7 @@ def test_node_install_unix(tmp_path, mocker):
     mocker.patch("httpx.get", return_value=Resp())
     download = mocker.patch("reflex.utils.prerequisites.download_and_run")
     mocker.patch("reflex.utils.processes.new_process")
-    mocker.patch("reflex.utils.processes.show_logs")
+    mocker.patch("reflex.utils.processes.stream_logs")
 
     prerequisites.install_node()
 
