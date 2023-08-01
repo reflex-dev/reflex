10c8d5aecd36435b2aeeab0749477d8de296c5af
Masen Furer <m_github@0x26.net>
2023-08-01 12:40:35 -0700

    scripts/integration: pass arbitrary parameters to reflex run
    Additionally accept a third argument as the ports to check. This change allows
    integration tests to pass `--frontend-only` or `--backend-only` and check that
    the proper ports are listening.

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 # Change directory to the first argument passed to the script
-pushd "$1" || exit 1
-echo "Changed directory to $1"
+project_dir=$1
+shift
+pushd "$project_dir" || exit 1
+echo "Changed directory to $project_dir"
 
 # So we get stdout / stderr from Python ASAP. Without this, delays can be very long (e.g. on Windows, Github Actions)
 export PYTHONUNBUFFERED=1
 
+env_mode=$1
+shift
+check_ports=${1:-3000 8000}
+shift
+
 # Start the server in the background
-reflex run --loglevel debug --env "$2" & pid=$!
+reflex run --loglevel debug --env "$env_mode" "$@" & pid=$!
 
 # TODO does this even work on windows? Not clear, possibly not impactful though.
 trap "kill -INT $pid ||:" EXIT
@@ -17,4 +24,4 @@ echo "Started server with PID $pid"
 
 # Assume we run from the root of the repo
 popd
-python scripts/wait_for_listening_port.py 3000 8000 --timeout=600 --server-pid "$pid"
+python scripts/wait_for_listening_port.py $check_ports --timeout=600 --server-pid "$pid"
