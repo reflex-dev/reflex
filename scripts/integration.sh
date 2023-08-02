c56d35ea37a7e71847a3a3372c1dc3b88c0c3c25
Masen Furer <m_github@0x26.net>
2023-08-02 09:39:48 -0700

    scripts/integration: fix mismerge -- wait for $check_ports
    error introduced via https://github.com/reflex-dev/reflex/pull/1478/commits/1e2a5aad0ba6b53e8ee3b0609f72146bf8b17f0b

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -33,4 +33,4 @@ if [ -f /proc/$pid/winpid ]; then
   echo "Windows detected, passing winpid $pid to port waiter"
 fi
 
-python scripts/wait_for_listening_port.py 3000 8000 --timeout=600 --server-pid "$pid"
+python scripts/wait_for_listening_port.py $check_ports --timeout=600 --server-pid "$pid"
