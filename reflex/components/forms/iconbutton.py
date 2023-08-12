97b370ec5dcc944a92abf480cd299d61c81b2b33
andrewbantly <andrewbantly@gmail.com>
2023-08-11 17:55:49 -0700

    icon button renders correctly, aria-label rendering as arialabel

--- reflex/components/forms/iconbutton.py
+++ reflex/components/forms/iconbutton.py
@@ -1,14 +1,43 @@
 from reflex.components import Component
 from reflex.components.media.icon import Icon
+from reflex.vars import Var
 
 class ChakraIconButtonComponent(Component):
     """A component that wraps a Chakra icon button component."""
     
     library = "@chakra-ui/react"
+    
 
 class IconButton(ChakraIconButtonComponent):
     """An icon button component."""
+   
     tag = "IconButton"
+
+    # The type of button.
+    type: Var[str]
+
+    #  A label that describes the button
+    aria_label: Var[str]
+
+    # aria-label: Var[str]
+
+    # The icon to be used in the button.
+    icon: Var[str]
+
+    # If true, the button will be styled in its active state.
+    is_active: Var[bool]
+
+    # If true, the button will be disabled.
+    is_disabled: Var[bool]
+
+    # If true, the button will show a spinner.
+    is_loading: Var[bool]
+
+    # If true, the button will be perfectly round. Else, it'll be slightly round
+    is_round: Var[bool]
+
+    # Replace the spinner component when isLoading is set to true
+    spinner: Var[str]
 
     @classmethod
     def create(cls, **props):
@@ -19,6 +48,5 @@ class IconButton(ChakraIconButtonComponent):
 
         # Get the Chakra UI's icon component instance
         icon_instance = Icon.create(tag=props.pop("icon"))
-        print("tag:",icon_instance.tag)
-        # Return the IconButton component using the tag of icon_instance instead of the instance itself
-        return super().create(icon=icon_instance, aria_label=props.pop("aria_label"), **props)
+
+        return super().create(icon_instance, **props)
