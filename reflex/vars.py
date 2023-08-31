cc89f2b6e7815e7219cd74b0d546eefdac9b6930
Masen Furer <m_github@0x26.net>
2023-08-31 09:50:15 -0700

    Proper serialization for chained Event payloads (#1725)

--- reflex/vars.py
+++ reflex/vars.py
@@ -144,6 +144,24 @@ class Var(ABC):
         """
         return _GenericAlias(cls, type_)
 
+    def _decode(self) -> Any:
+        """Decode Var as a python value.
+
+        Note that Var with state set cannot be decoded python-side and will be
+        returned as full_name.
+
+        Returns:
+            The decoded value or the Var name.
+        """
+        if self.state:
+            return self.full_name
+        if self.is_string or self.type_ is Figure:
+            return self.name
+        try:
+            return json.loads(self.name)
+        except ValueError:
+            return self.name
+
     def equals(self, other: Var) -> bool:
         """Check if two vars are equal.
 
