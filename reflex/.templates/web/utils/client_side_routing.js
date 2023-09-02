f77cd31dbb5553fc5fe75da1b9314a72de9190cd
Masen Furer <m_github@0x26.net>
2023-09-02 08:09:28 -0700

    Update comments after cleaning up client_side_routing.js

--- reflex/.templates/web/utils/client_side_routing.js
+++ reflex/.templates/web/utils/client_side_routing.js
@@ -2,13 +2,10 @@ import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
 
 /**
- * React hook for use in page components to enable client-side routing.
+ * React hook for use in /404 page to enable client-side routing.
  *
  * Uses the next/router to redirect to the provided URL when loading
  * the 404 page (for example as a fallback in static hosting situations).
- * Resets the `routeNotFound` state when navigating to a non-404 page.
- * Registers for `routeChangeComplete` events to re-hydrate the state when
- * navigating between pages.
  *
  * @returns {boolean} routeNotFound - true if the current route is an actual 404
  */
@@ -16,26 +13,24 @@ export const useClientSideRouting = () => {
   const [routeNotFound, setRouteNotFound] = useState(false)
   const didRedirect = useRef(false)
   const router = useRouter()
-  useEffect(() => {  // Effect performs the actual redirection
+  useEffect(() => {
     if (
       router.isReady &&
       !didRedirect.current  // have not tried redirecting yet
     ) {
+      didRedirect.current = true  // never redirect twice to avoid "Hard Navigate" error
       // attempt to redirect to the route in the browser address bar once
-      didRedirect.current = true
       router.replace({
           pathname: window.location.pathname,
           query: window.location.search.slice(1),
       })
       .catch((e) => {
-        // When navigation fails, set routeNotFound to true to avoid redirecting again,
-        // which avoids hitting the "Hard Navigate" invariant error.
-        setRouteNotFound(true)
+        setRouteNotFound(true)  // navigation failed, so this is a real 404
       })
     }
   }, [router.isReady]);
 
-  // Return the reactive bool, so we can avoid flashing 404 page until we know
-  // for sure the route is not found.
+  // Return the reactive bool, to avoid flashing 404 page until we know for sure
+  // the route is not found.
   return routeNotFound
 }
