f80850af234e2f8f559743aa69e48533b556445c
Masen Furer <m_github@0x26.net>
2023-09-02 08:25:57 -0700

    templates: remove unused formatting const

--- reflex/compiler/templates.py
+++ reflex/compiler/templates.py
@@ -39,7 +39,6 @@ class ReflexJinjaEnvironment(Environment):
             "toggle_color_mode": constants.TOGGLE_COLOR_MODE,
             "use_color_mode": constants.USE_COLOR_MODE,
             "hydrate": constants.HYDRATE,
-            "route_not_found": constants.ROUTE_NOT_FOUND,
         }
 
 
