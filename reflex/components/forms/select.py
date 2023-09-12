77edb01e746530eeefa5ebaef1444851b3fe230c
wassaf shahzad <wassafshahzad@gmail.com>
2023-09-12 08:38:49 -0700

    removed is_read_only from select (#1799)

--- reflex/components/forms/select.py
+++ reflex/components/forms/select.py
@@ -36,9 +36,6 @@ class Select(ChakraComponent):
     # If true, the form control will be invalid. This has 2 side effects: - The FormLabel and FormErrorIcon will have `data-invalid` set to true - The form element (e.g, Input) will have `aria-invalid` set to true
     is_invalid: Var[bool]
 
-    # If true, the form control will be readonly
-    is_read_only: Var[bool]
-
     # If true, the form control will be required. This has 2 side effects: - The FormLabel will show a required indicator - The form element (e.g, Input) will have `aria-required` set to true
     is_required: Var[bool]
 
