abb15e4a90438c043b2018a9e70824707dcd91fa
Masen Furer <m_github@0x26.net>
2023-10-01 09:23:38 -0700

    to_snake_case is defined in reflex.utils.format

--- reflex/components/__init__.py
+++ reflex/components/__init__.py
@@ -1,6 +1,8 @@
 """Import all the components."""
 from __future__ import annotations
 
+import reflex.utils.format
+
 from .base import Script
 from .component import Component
 from .component import NoSSRComponent as NoSSRComponent
@@ -14,12 +16,11 @@ from .media import *
 from .navigation import *
 from .overlay import *
 from .typography import *
-import utils
 
 # Dynamic Convenience methods for all the components.
 locals().update(
     {
-        utils.to_snake_case(name): value.create
+        reflex.utils.format.to_snake_case(name): value.create
         for name, value in locals().items()
         if isinstance(value, type) and issubclass(value, Component)
     }
