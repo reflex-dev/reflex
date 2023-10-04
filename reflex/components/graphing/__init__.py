f4606795d3b1c1f2deef768f3c8e9837293efa72
Alek Petuskey <alek@pynecone.io>
2023-10-04 15:05:08 -0700

    Core Graphing -> Recharts (#1878)

--- reflex/components/graphing/__init__.py
+++ reflex/components/graphing/__init__.py
@@ -1,21 +1,6 @@
 """Convenience functions to define layout components."""
 
 from .plotly import Plotly
-from .victory import (
-    Area,
-    Bar,
-    BoxPlot,
-    Candlestick,
-    Chart,
-    ChartGroup,
-    ChartStack,
-    ErrorBar,
-    Histogram,
-    Line,
-    Pie,
-    Polar,
-    Scatter,
-    Voronoi,
-)
+from .recharts import *
 
 __all__ = [f for f in dir() if f[0].isupper()]  # type: ignore
---
+++ reflex/components/graphing/recharts/__init__.py
@@ -0,0 +1,41 @@
+"""Recharts components."""
+
+from .cartesian import (
+    Area,
+    Bar,
+    Brush,
+    Cartesian,
+    CartesianAxis,
+    CartesianGrid,
+    ErrorBar,
+    Funnel,
+    Line,
+    ReferenceArea,
+    ReferenceDot,
+    ReferenceLine,
+    Scatter,
+    XAxis,
+    YAxis,
+    ZAxis,
+)
+from .charts import (
+    AreaChart,
+    BarChart,
+    ComposedChart,
+    FunnelChart,
+    LineChart,
+    PieChart,
+    RadarChart,
+    RadialBarChart,
+    ScatterChart,
+    Treemap,
+)
+from .general import GraphingTooltip, Label, LabelList, Legend, ResponsiveContainer
+from .polar import (
+    Pie,
+    PolarAngleAxis,
+    PolarGrid,
+    PolarRadiusAxis,
+    Radar,
+    RadialBar,
+)
