2785dd9c66a929d22e46b3595aa4ce464496d718
Masen Furer <m_github@0x26.net>
2023-10-11 15:14:45 -0700

    Finish wrapping Button, Switch, and TextField components

--- reflex/components/radix/themes/components.py
+++ reflex/components/radix/themes/components.py
@@ -1,17 +1,107 @@
-from .base import RadixThemesComponent
+"""Interactive components provided by @radix-ui/themes."""
+from reflex.vars import Var
+
+from .base import CommonMarginProps, RadixThemesComponent
 
 
-class Button(RadixThemesComponent):
+class Button(CommonMarginProps, RadixThemesComponent):
+    """Trigger an action or event, such as submitting a form or displaying a dialog."""
+
     tag = "Button"
 
+    # Change the default rendered element for the one passed as a child, merging their props and behavior.
+    as_child: Var[bool]
 
-class Select(RadixThemesComponent):
-    tag = "Select"
+    # Button size "1" - "4"
+    size: Var[str]
+
+    # Variant of button: "solid" | "soft" | "outline" | "ghost"
+    variant: Var[str]
+
+    # Override theme color for button
+    color: Var[str]
+
+    # Whether to render the button with higher contrast color against background
+    high_contrast: Var[bool]
+
+    # Override theme radius for button: "none" | "small" | "medium" | "large" | "full"
+    radius: Var[str]
 
 
-class Switch(RadixThemesComponent):
+class Switch(CommonMarginProps, RadixThemesComponent):
+    """A toggle switch alternative to the checkbox."""
+
     tag = "Switch"
 
+    # Change the default rendered element for the one passed as a child, merging their props and behavior.
+    as_child: Var[bool]
 
-class TextField(RadixThemesComponent):
+    # Whether the switch is checked by default
+    default_checked: Var[bool]
+
+    # Whether the switch is checked
+    checked: Var[bool]
+
+    # If true, prevent the user from interacting with the switch
+    disabled: Var[bool]
+
+    # If true, the user must interact with the switch to submit the form
+    required: Var[bool]
+
+    # The name of the switch (when submitting a form)
+    name: Var[str]
+
+    # The value associated with the "on" position
+    value: Var[str]
+
+    # Switch size "1" - "4"
+    size: Var[str]
+
+    # Variant of switch: "solid" | "soft" | "outline" | "ghost"
+    variant: Var[str]
+
+    # Override theme color for switch
+    color: Var[str]
+
+    # Whether to render the switch with higher contrast color against background
+    high_contrast: Var[bool]
+
+    # Override theme radius for switch: "none" | "small" | "medium" | "large" | "full"
+    radius: Var[str]
+
+
+class TextFieldRoot(CommonMarginProps, RadixThemesComponent):
+    """Captures user input with an optional slot for buttons and icons."""
+
+    tag = "TextField.Root"
+
+    # Button size "1" - "3"
+    size: Var[str]
+
+    # Variant of text field: "classic" | "surface" | "soft"
+    variant: Var[str]
+
+    # Override theme color for text field
+    color: Var[str]
+
+    # Override theme radius for text field: "none" | "small" | "medium" | "large" | "full"
+    radius: Var[str]
+
+
+class TextField(TextFieldRoot):
+    """The input part of a TextField, may be used by itself."""
+
     tag = "TextField.Input"
+
+
+TextFieldInput = TextField
+
+
+class TextFieldSlot(RadixThemesComponent):
+    """Contains icons or buttons associated with an Input."""
+
+    # Override theme color for text field slot
+    color: Var[str]
+
+    # Override the gap spacing between slot and input: "1" - "9"
+    gap: Var[str]
