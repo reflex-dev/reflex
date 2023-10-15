8bcd840bc9f62d6b05be4147e9c2c6d2b28dd8ff
Alek Petuskey <alekpetuskey@aleks-mbp.lan>
2023-10-15 16:39:30 -0700

    Added Project dir to bash

--- scripts/lighthouse.sh
+++ scripts/lighthouse.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Change directory to the first argument passed to the script
+project_dir=$1
+shift
+pushd "$project_dir" || exit 1
+echo "Changed directory to $project_dir"
+
 cat << EOF > lighthouserc.js
 module.exports = {
   ci: {
