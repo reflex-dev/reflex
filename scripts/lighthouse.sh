6003a9c0cf5bfb81b4b9a269b52a6db3df4a0687
Alek Petuskey <alekpetuskey@aleks-mbp.lan>
2023-10-15 17:28:33 -0700

    Refactor Check

--- scripts/lighthouse.sh
+++ scripts/lighthouse.sh
@@ -6,6 +6,7 @@ shift
 pushd "$project_dir" || exit 1
 echo "Changed directory to $project_dir"
 
+# Create a lighthouserc.js file
 cat << EOF > lighthouserc.js
 module.exports = {
   ci: {
@@ -20,3 +21,38 @@ module.exports = {
   },
 };
 EOF
+
+# So we get stdout / stderr from Python ASAP. Without this, delays can be very long (e.g. on Windows, Github Actions)
+export PYTHONUNBUFFERED=1
+
+env_mode=$1
+shift
+check_ports=${1:-3000 8000}
+shift
+
+# Start the server in the background
+export TELEMETRY_ENABLED=false
+reflex run --loglevel debug --env "$env_mode" "$@" & pid=$!
+
+# Within the context of this bash, $pid_in_bash is what we need to pass to "kill" on exit
+# This is true on all platforms.
+pid_in_bash=$pid
+trap "kill -INT $pid_in_bash ||:" EXIT
+
+echo "Started server with PID $pid"
+
+# Change to .web directory
+cd .web
+npm install -g @lhci/cli@0.3.x
+lhci autorun --upload.target=temporary-public-storage || echo "LHCI failed!"
+
+# Assume we run from the root of the repo
+popd
+
+# In Windows, our Python script below needs to work with the WINPID
+if [ -f /proc/$pid/winpid ]; then
+  pid=$(cat /proc/$pid/winpid)
+  echo "Windows detected, passing winpid $pid to port waiter"
+fi
+
+python scripts/wait_for_listening_port.py $check_ports --timeout=600 --server-pid "$pid"
