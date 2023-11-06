3c11190d378e1ae480c0a966453c6b5d9c81438d
Lendemor <thomas.brandeho@gmail.com>
2023-11-06 13:42:14 +0100

    fix to_snake_case and add tests

--- reflex/utils/format.py
+++ reflex/utils/format.py
@@ -122,7 +122,7 @@ def to_snake_case(text: str) -> str:
         The snake case string.
     """
     s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", text)
-    return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
+    return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower().replace("-", "_")
 
 
 def to_camel_case(text: str) -> str:
