720833118c567d7e0e198e48f9d3002b3ef29b7b
Elijah <elijahahianyo@gmail.com>
2023-11-08 15:33:10 +0000

    add node bin to path of wsl1

--- reflex/utils/processes.py
+++ reflex/utils/processes.py
@@ -127,7 +127,7 @@ def new_process(args, run: bool = False, show_logs: bool = False, **kwargs):
         Execute a child program in a new process.
     """
     node_bin_path = ""
-    if not constants.IS_LINUX_OR_MAC:
+    if not constants.IS_LINUX_OR_MAC or not prerequisites.is_valid_linux():
         node_bin_path = path_ops.get_node_bin_path()
         if not node_bin_path:
             console.warn(
