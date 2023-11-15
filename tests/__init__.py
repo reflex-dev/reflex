7bc3c712a2498ce0fb11ac8ffa15468854182ba9
Elijah <elijahahianyo@gmail.com>
2023-11-15 19:56:11 +0000

    remove debug lines

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,3 @@
 import os
 
 from reflex import constants
-
-os.environ[constants.PYTEST_CURRENT_TEST] = "true"
-print("Gegining testing-------------\n")
-print(constants.PYTEST_CURRENT_TEST in os.environ)
