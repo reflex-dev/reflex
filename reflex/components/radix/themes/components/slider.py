c1a6088536ebfcfaf883e0f10c4196e5241bbfdb
Masen Furer <m_github@0x26.net>
2024-01-11 16:40:29 -0800

    Correct Radix Themes Slider props
    All number-like props should accept either float or int types.
    
    The `value` props must take a list of values.

--- reflex/components/radix/themes/components/slider.py
+++ reflex/components/radix/themes/components/slider.py
@@ -1,5 +1,5 @@
 """Interactive components provided by @radix-ui/themes."""
-from typing import Any, Dict, List, Literal
+from typing import Any, Dict, List, Literal, Union
 
 from reflex.vars import Var
 
@@ -35,22 +35,22 @@ class Slider(CommonMarginProps, RadixThemesComponent):
     radius: Var[LiteralRadius]
 
     # The value of the slider when initially rendered. Use when you do not need to control the state of the slider.
-    default_value: Var[List[float]]
+    default_value: Var[List[Union[float, int]]]
 
     # The controlled value of the slider. Must be used in conjunction with onValueChange.
-    value: Var[float]
+    value: Var[List[Union[float, int]]]
 
     # The name of the slider. Submitted with its owning form as part of a name/value pair.
     name: Var[str]
 
     # The minimum value of the slider.
-    min: Var[float, int]
+    min: Var[Union[float, int]]
 
     # The maximum value of the slider.
-    max: Var[float, int]
+    max: Var[Union[float, int]]
 
     # The step value of the slider.
-    step: Var[float, int]
+    step: Var[Union[float, int]]
 
     # Whether the slider is disabled
     disabled: Var[bool]
