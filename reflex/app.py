7ae02b7b7cbf44abedd05ca820dad3e1e4a9b3dd
Nikhil Rao <nikhil@reflex.dev>
2024-01-18 12:10:01 -0800

    Fix precommit

--- reflex/app.py
+++ reflex/app.py
@@ -46,7 +46,6 @@ from reflex.components.core.client_side_routing import (
     Default404Page,
     wait_for_client_redirect,
 )
-from reflex.components.radix import themes
 from reflex.config import get_config
 from reflex.event import Event, EventHandler, EventSpec
 from reflex.middleware import HydrateMiddleware, Middleware
