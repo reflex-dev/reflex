99bd5e5fd87bc2d50dd9159eef333bb0a5d88003
Nikhil Rao <nikhil@reflex.dev>
2024-01-17 19:10:22 -0800

    High level api for tabs component

--- reflex/components/radix/themes/components/tabs.py
+++ reflex/components/radix/themes/components/tabs.py
@@ -1,6 +1,8 @@
 """Interactive components provided by @radix-ui/themes."""
-from typing import Any, Dict, Literal
+from typing import Any, Dict, List, Literal, Union
 
+import reflex as rx
+from reflex.components.radix.themes.layout.box import Box
 from reflex.vars import Var
 
 from ..base import (
@@ -22,9 +24,6 @@ class TabsRoot(CommonMarginProps, RadixThemesComponent):
 
     # The controlled value of the tab that should be active. Use when you need to control the state of the tabs.
     value: Var[str]
-
-    # The orientation of the tabs.
-    orientation: Var[Literal["horizontal", "vertical"]]
 
     def get_event_triggers(self) -> Dict[str, Any]:
         """Get the events triggers signatures for the component.
@@ -63,3 +62,65 @@ class TabsContent(CommonMarginProps, RadixThemesComponent):
 
     # The value of the tab. Must be unique for each tab.
     value: Var[str]
+
+
+class Tabs(TabsRoot):
+    """High level component to render a Tabs component."""
+
+    # The labels of the tabs.
+    labels: Var[List[str]]
+
+    # The content to render.
+    contents: List[rx.Component]
+
+    @classmethod
+    def create(
+        cls,
+        labels: Union[List[str], Var[List[str]]],
+        contents: Union[Var[List[str]], List[rx.Component]],
+        **props
+    ) -> rx.Component:
+        """Create a Tabs component.
+
+        Args:
+            labels: The labels of the tabs.
+            contents: The contents of the tabs.
+            **props: The properties of the component.
+
+        Returns:
+            The component.
+        """
+        labels2 = Var.create_safe(labels).to(list[str])
+        contents = (
+            [
+                rx.foreach(
+                    contents,
+                    lambda content, i: TabsContent.create(content, value=labels[i]),
+                )
+            ]
+            if isinstance(contents, Var)
+            else [
+                TabsContent.create(content, value=labels[i])
+                for i, content in enumerate(contents)
+            ]
+        )
+
+        print(str(contents[0]))
+        return TabsRoot.create(
+            TabsList.create(
+                rx.foreach(
+                    labels2,
+                    lambda label: TabsTrigger.create(label, value=label),
+                ),
+            ),
+            Box.create(
+                *contents,
+                px="4",
+                pt="3",
+                pb="2",
+            ),
+            **props,
+        )
+
+
+tabs = Tabs.create
