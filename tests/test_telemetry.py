eae2529e5ddb608157868ee0edc32647ac8ccba2
Yummy-Yums <eliasderby90@gmail.com>
2024-01-23 15:05:45 +0000

    ruff updated formatting

--- tests/test_telemetry.py
+++ tests/test_telemetry.py
@@ -1,9 +1,6 @@
+from packaging.version import parse as parse_python_version
+
 from reflex.utils import telemetry
-
-
-def versiontuple(v):
-    whatTosplit = (v.split("."))[:-1]
-    return tuple(map(int, whatTosplit))
 
 
 def test_telemetry():
@@ -25,7 +22,7 @@ def test_telemetry():
     # Check that the Python version is greater than 3.7.
     python_version = telemetry.get_python_version()
     assert python_version is not None
-    assert versiontuple(python_version) >= versiontuple("3.7")
+    assert parse_python_version(python_version) >= parse_python_version("3.7")
 
 
 def test_disable():
