515706855ee8fa6a308e5069642a638d6ef7ce38
Alek Petuskey <alekpetuskey@aleks-mbp.lan>
2024-01-23 21:11:51 -0800

    Improvments

--- reflex/constants/colors.py
+++ reflex/constants/colors.py
@@ -1,5 +1,9 @@
 """The colors used in Reflex are a wrapper around https://www.radix-ui.com/colors."""
 from typing import Literal
+from reflex.vars import Var
+from reflex.base import Base
+from reflex.utils.serializers import SerializedType, serialize, serializer
+from dataclasses import dataclass
 
 ColorType= Literal[
         "gray",
@@ -38,20 +42,34 @@ ColorType= Literal[
 
 ShadeType= Literal[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-
+@dataclass
 class Colors:
-    def __init__(self, color: ColorType, shade: ShadeType, alpha: bool = False) -> None:
-        self.color = color
-        self.shade = shade
-        self.alpha = alpha
+    """A color in the Reflex color palette."""
 
-    def __str__(self) -> str:
-        if self.alpha:
-            return f"--{self.color}-{self.shade}-alpha"
-        else:
-            return f"--var({self.color}-{self.shade})"
+    # The color palette to use
+    color: ColorType
 
-        
+    # The shade of the color to use
+    shade: ShadeType = 7
+
+    # Whether to use the alpha variant of the color
+    alpha: bool = False
+
+@serializer
+def serialize_color(color: Colors) -> SerializedType:
+    """Serialize a color.
+
+    Args:
+        color: The color to serialize.
+
+    Returns:
+        The serialized color.
+    """
+    print(f"--var({color.color}-{color.shade})")
+    if color.alpha:
+        return f"var(--{color.color}-{color.shade}-alpha)"
+    else:
+        return f"var(--{color.color}-{color.shade})"
 
     
 
