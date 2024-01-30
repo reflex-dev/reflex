81697c680a5fe877b83ccaba0c2b4a8ef4085c70
Lendemor <thomas.brandeho@gmail.com>
2024-01-30 20:13:57 +0100

    fix apply_theme on other components

--- reflex/components/lucide/icon.py
+++ reflex/components/lucide/icon.py
@@ -59,7 +59,7 @@ class Icon(LucideIconComponent):
         props["alias"] = f"Lucide{props['tag']}"
         return super().create(*children, **props)
 
-    def _apply_theme(self, theme: Component | None):
+    def _apply_theme(self, theme: Component):
         self.style = Style(
             {
                 "color": f"var(--accent-12)",
