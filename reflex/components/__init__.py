99e5668347e3936f8412d0b3ca77080ff55770ef
Masen Furer <m_github@0x26.net>
2024-02-08 12:52:00 -0800

    Use rx.el.img as rx.image

--- reflex/components/__init__.py
+++ reflex/components/__init__.py
@@ -7,13 +7,16 @@ from .component import Component
 from .component import NoSSRComponent as NoSSRComponent
 from .core import *
 from .datadisplay import *
+from .el.elements.media import Img
 from .gridjs import *
 from .markdown import *
 from .moment import *
-from .next import NextLink, image, next_link
+from .next import NextLink, next_link
 from .plotly import *
 from .radix import *
 from .react_player import *
 from .suneditor import *
 
 icon = lucide.icon
+image = Img.create
+del Img
