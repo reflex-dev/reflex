93c0091aadb645c70c4c867791e97aab412a1427
Nikhil Rao <nikhil@reflex.dev>
2024-02-13 01:54:58 +0700

    remove extra props on scroll area (#2581)

--- reflex/components/radix/themes/components/scrollarea.py
+++ reflex/components/radix/themes/components/scrollarea.py
@@ -4,7 +4,6 @@ from typing import Literal
 from reflex.vars import Var
 
 from ..base import (
-    LiteralRadius,
     RadixThemesComponent,
 )
 
@@ -13,12 +12,6 @@ class ScrollArea(RadixThemesComponent):
     """Custom styled, cross-browser scrollable area using native functionality."""
 
     tag = "ScrollArea"
-
-    # The size of the radio group: "1" | "2" | "3"
-    size: Var[Literal[1, 2, 3]]
-
-    # The radius of the radio group
-    radius: Var[LiteralRadius]
 
     # The alignment of the scroll area
     scrollbars: Var[Literal["vertical", "horizontal", "both"]]
