a85b26479d3e2fcd6db3bb1c628ad9bd9067997f
Masen Furer <m_github@0x26.net>
2024-03-04 16:10:25 -0800

    remove _is_dev_mode and replace it with calls to is_prod_mode()
    _is_dev_mode was a private function in the compiler, but now that utils.exec
    exposes is_prod_mode, we should use that throughout for consistency

--- reflex/compiler/compiler.py
+++ reflex/compiler/compiler.py
@@ -66,10 +66,6 @@ def _compile_theme(theme: dict) -> str:
     return templates.THEME.render(theme=theme)
 
 
-def _is_dev_mode() -> bool:
-    return not is_prod_mode()
-
-
 def _compile_contexts(state: Optional[Type[BaseState]], theme: Component) -> str:
     """Compile the initial state and contexts.
 
@@ -88,12 +84,12 @@ def _compile_contexts(state: Optional[Type[BaseState]], theme: Component) -> str
             initial_state=utils.compile_state(state),
             state_name=state.get_name(),
             client_storage=utils.compile_client_storage(state),
-            is_dev_mode=_is_dev_mode(),
+            is_dev_mode=not is_prod_mode(),
             default_color_mode=appearance,
         )
         if state
         else templates.CONTEXT.render(
-            is_dev_mode=_is_dev_mode(),
+            is_dev_mode=not is_prod_mode(),
             default_color_mode=appearance,
         )
     )
@@ -256,7 +252,7 @@ def _compile_stateful_components(
         if (
             isinstance(component, StatefulComponent)
             and component.references > 1
-            and not _is_dev_mode()
+            and is_prod_mode()
         ):
             # Reset this flag to render the actual component.
             component.rendered_as_shared = False
@@ -476,7 +472,7 @@ def remove_tailwind_from_postcss() -> tuple[str, str]:
 
 def purge_web_pages_dir():
     """Empty out .web/pages directory."""
-    if _is_dev_mode() and os.environ.get("REFLEX_PERSIST_WEB_DIR"):
+    if not is_prod_mode() and os.environ.get("REFLEX_PERSIST_WEB_DIR"):
         # Skip purging the web directory in dev mode if REFLEX_PERSIST_WEB_DIR is set.
         return
 
