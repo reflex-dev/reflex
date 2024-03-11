9358cb0305b1ff5b1946518445d6377fcbfbb7ed
Nikhil Rao <nikhil@reflex.dev>
2024-03-11 15:38:52 -0700

    update pyi

--- reflex/components/radix/themes/components/select.pyi
+++ reflex/components/radix/themes/components/select.pyi
@@ -863,16 +863,12 @@ class HighLevelSelect(SelectRoot):
                 Literal["none", "small", "medium", "large", "full"],
             ]
         ] = None,
-<<<<<<< HEAD
-        width: Optional[Union[Var[str], str]] = None,
         position: Optional[
             Union[
                 Var[Literal["item-aligned", "popper"]],
                 Literal["item-aligned", "popper"],
             ]
         ] = None,
-=======
->>>>>>> 217345f6 (Remove width prop from rx.select)
         size: Optional[
             Union[Var[Literal["1", "2", "3"]], Literal["1", "2", "3"]]
         ] = None,
@@ -953,11 +949,7 @@ class HighLevelSelect(SelectRoot):
             high_contrast: Whether to render the select with higher contrast color against background.
             variant: The variant of the select.
             radius: The radius of the select.
-<<<<<<< HEAD
-            width: The width of the select.
             position: The positioning mode to use. Default is "item-aligned".
-=======
->>>>>>> 217345f6 (Remove width prop from rx.select)
             size: The size of the select: "1" | "2" | "3"
             default_value: The value of the select when initially rendered. Use when you do not need to control the state of the select.
             value: The controlled value of the select. Should be used in conjunction with on_change.
@@ -1069,16 +1061,12 @@ class Select(ComponentNamespace):
                 Literal["none", "small", "medium", "large", "full"],
             ]
         ] = None,
-<<<<<<< HEAD
-        width: Optional[Union[Var[str], str]] = None,
         position: Optional[
             Union[
                 Var[Literal["item-aligned", "popper"]],
                 Literal["item-aligned", "popper"],
             ]
         ] = None,
-=======
->>>>>>> 217345f6 (Remove width prop from rx.select)
         size: Optional[
             Union[Var[Literal["1", "2", "3"]], Literal["1", "2", "3"]]
         ] = None,
@@ -1159,11 +1147,7 @@ class Select(ComponentNamespace):
             high_contrast: Whether to render the select with higher contrast color against background.
             variant: The variant of the select.
             radius: The radius of the select.
-<<<<<<< HEAD
-            width: The width of the select.
             position: The positioning mode to use. Default is "item-aligned".
-=======
->>>>>>> 217345f6 (Remove width prop from rx.select)
             size: The size of the select: "1" | "2" | "3"
             default_value: The value of the select when initially rendered. Use when you do not need to control the state of the select.
             value: The controlled value of the select. Should be used in conjunction with on_change.
