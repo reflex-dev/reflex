8bd6051ad702c0ac0c2180ae300f70e22027cf2f
Lendemor <thomas.brandeho@gmail.com>
2024-03-29 11:56:59 +0100

    add warning when using rx._x

--- reflex/experimental/__init__.py
+++ reflex/experimental/__init__.py
@@ -2,7 +2,12 @@
 
 from types import SimpleNamespace
 
+from ..utils.console import warn
 from . import hooks as hooks
+
+warn(
+    "`rx._x` contains experimental features and might be removed at any time in the future .",
+)
 
 _x = SimpleNamespace(
     hooks=hooks,
