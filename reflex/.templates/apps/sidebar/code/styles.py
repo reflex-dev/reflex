0981ac675bf0c31e4186ba88bce5aac0ccfd1d3e
Alek Petuskey <alek@pynecone.io>
2024-04-02 11:48:34 -0700

    Update sidebar template (#2959)

--- reflex/.templates/apps/sidebar/code/styles.py
+++ reflex/.templates/apps/sidebar/code/styles.py
@@ -3,8 +3,7 @@
 import reflex as rx
 
 border_radius = "0.375rem"
-box_shadow = "0px 0px 0px 1px rgba(84, 82, 95, 0.14)"
-border = f"1px solid {rx.color('accent', 12)}"
+border = f"1px solid {rx.color('gray', 6)}"
 text_color = rx.color("gray", 11)
 accent_text_color = rx.color("accent", 10)
 accent_color = rx.color("accent", 1)
@@ -17,7 +16,6 @@ template_page_style = {"padding_top": "5em", "padding_x": ["auto", "2em"], "flex
 
 template_content_style = {
     "align_items": "flex-start",
-    "box_shadow": box_shadow,
     "border_radius": border_radius,
     "padding": "1em",
     "margin_bottom": "2em",
@@ -31,30 +29,17 @@ link_style = {
 
 overlapping_button_style = {
     "background_color": "white",
-    "border": border,
     "border_radius": border_radius,
 }
 
-base_style = {
-    rx.menu.trigger: {
-        **overlapping_button_style,
-    },
-    rx.menu.item: hover_accent_bg,
-}
-
 markdown_style = {
-    "code": lambda text: rx.code(text, color=accent_text_color, bg=accent_color),
+    "code": lambda text: rx.code(text, color_scheme="gray"),
+    "codeblock": lambda text, **props: rx.code_block(text, **props, margin_y="1em"),
     "a": lambda text, **props: rx.link(
         text,
         **props,
         font_weight="bold",
-        color=accent_text_color,
         text_decoration="underline",
         text_decoration_color=accent_text_color,
-        _hover={
-            "color": accent_color,
-            "text_decoration": "underline",
-            "text_decoration_color": accent_color,
-        },
     ),
 }
