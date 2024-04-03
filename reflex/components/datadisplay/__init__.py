dad9d0b4d63c0032c49e22be5bffaca26dd5c277
Tom Gotsman <tomgotsman@toms-mbp.lan>
2024-04-03 11:58:16 -0700

    update to a function

--- reflex/components/datadisplay/__init__.py
+++ reflex/components/datadisplay/__init__.py
@@ -4,9 +4,8 @@ from .code import CodeBlock
 from .code import LiteralCodeBlockTheme as LiteralCodeBlockTheme
 from .code import LiteralCodeLanguage as LiteralCodeLanguage
 from .dataeditor import DataEditor, DataEditorTheme
-from .logo import Logo
+from .logo import logo
 
 code_block = CodeBlock.create
 data_editor = DataEditor.create
 data_editor_theme = DataEditorTheme
-logo = Logo.create
