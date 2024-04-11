3733e7045bc64537af1bbcf9e2b9e2ebe039bd4b
Lendemor <thomas.brandeho@gmail.com>
2024-04-12 00:49:28 +0200

    add run_in_thread helper function

--- reflex/experimental/__init__.py
+++ reflex/experimental/__init__.py
@@ -4,6 +4,7 @@ from types import SimpleNamespace
 
 from ..utils.console import warn
 from . import hooks as hooks
+from .misc import run_in_thread as run_in_thread
 
 warn(
     "`rx._x` contains experimental features and might be removed at any time in the future .",
@@ -11,4 +12,5 @@ warn(
 
 _x = SimpleNamespace(
     hooks=hooks,
+    run_in_thread=run_in_thread,
 )
