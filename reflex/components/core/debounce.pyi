e8a5319e0dbbb3b6e5d8b9e8ea84ef3bda1e5763
Masen Furer <m_github@0x26.net>
2024-04-17 09:13:45 -0700

    Fix pyi files for debounce

--- reflex/components/core/debounce.pyi
+++ reflex/components/core/debounce.pyi
@@ -75,6 +75,9 @@ class DebounceInput(Component):
         on_mouse_up: Optional[
             Union[EventHandler, EventSpec, list, function, BaseVar]
         ] = None,
+        on_paste: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
         on_scroll: Optional[
             Union[EventHandler, EventSpec, list, function, BaseVar]
         ] = None,
