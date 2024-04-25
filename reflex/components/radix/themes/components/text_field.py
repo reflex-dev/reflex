b455135c8aac2093eba18aa781d95bbe83c98b4a
Nikhil Rao <nikhil@reflex.dev>
2024-04-24 18:38:40 -0700

    Fix app harness tests

--- reflex/components/radix/themes/components/text_field.py
+++ reflex/components/radix/themes/components/text_field.py
@@ -35,6 +35,39 @@ class TextFieldRoot(el.Div, RadixThemesComponent):
     # Override theme radius for text field: "none" | "small" | "medium" | "large" | "full"
     radius: Var[LiteralRadius]
 
+    # Whether the input should have autocomplete enabled
+    auto_complete: Var[bool]
+
+    # The value of the input when initially rendered.
+    default_value: Var[str]
+
+    # Disables the input
+    disabled: Var[bool]
+
+    # Specifies the maximum number of characters allowed in the input
+    max_length: Var[str]
+
+    # Specifies the minimum number of characters required in the input
+    min_length: Var[str]
+
+    # Name of the input, used when sending form data
+    name: Var[str]
+
+    # Placeholder text in the input
+    placeholder: Var[str]
+
+    # Indicates whether the input is read-only
+    read_only: Var[bool]
+
+    # Indicates that the input is required
+    required: Var[bool]
+
+    # Specifies the type of input
+    type: Var[str]
+
+    # Value of the input
+    value: Var[Union[str, int, float]]
+
     @classmethod
     def create(cls, *children, **props) -> Component:
         """Create an Input component.
@@ -76,89 +109,12 @@ class TextFieldSlot(RadixThemesComponent):
     color_scheme: Var[LiteralAccentColor]
 
 
-class Input(RadixThemesComponent):
-    """High level wrapper for the Input component."""
-
-    # Text field size "1" - "3"
-    size: Var[LiteralTextFieldSize]
-
-    # Variant of text field: "classic" | "surface" | "soft"
-    variant: Var[LiteralTextFieldVariant]
-
-    # Override theme color for text field
-    color_scheme: Var[LiteralAccentColor]
-
-    # Override theme radius for text field: "none" | "small" | "medium" | "large" | "full"
-    radius: Var[LiteralRadius]
-
-    # Whether the input should have autocomplete enabled
-    auto_complete: Var[bool]
-
-    # The value of the input when initially rendered.
-    default_value: Var[str]
-
-    # Disables the input
-    disabled: Var[bool]
-
-    # Specifies the maximum number of characters allowed in the input
-    max_length: Var[str]
-
-    # Specifies the minimum number of characters required in the input
-    min_length: Var[str]
-
-    # Name of the input, used when sending form data
-    name: Var[str]
-
-    # Placeholder text in the input
-    placeholder: Var[str]
-
-    # Indicates whether the input is read-only
-    read_only: Var[bool]
-
-    # Indicates that the input is required
-    required: Var[bool]
-
-    # Specifies the type of input
-    type: Var[str]
-
-    # Value of the input
-    value: Var[Union[str, int, float]]
-
-    @classmethod
-    def create(cls, **props):
-        """Create an Input component.
-
-        Args:
-            **props: The properties of the component.
-
-        Returns:
-            The component.
-        """
-        return TextFieldRoot.create(**props)
-
-    def get_event_triggers(self) -> Dict[str, Any]:
-        """Get the event triggers that pass the component's value to the handler.
-
-        Returns:
-            A dict mapping the event trigger to the var that is passed to the handler.
-        """
-        return {
-            **super().get_event_triggers(),
-            EventTriggers.ON_CHANGE: lambda e0: [e0.target.value],
-            EventTriggers.ON_FOCUS: lambda e0: [e0.target.value],
-            EventTriggers.ON_BLUR: lambda e0: [e0.target.value],
-            EventTriggers.ON_KEY_DOWN: lambda e0: [e0.key],
-            EventTriggers.ON_KEY_UP: lambda e0: [e0.key],
-        }
-
-
 class TextField(ComponentNamespace):
     """TextField components namespace."""
 
     root = staticmethod(TextFieldRoot.create)
-    # input = staticmethod(TextFieldInput.create)
     slot = staticmethod(TextFieldSlot.create)
-    __call__ = staticmethod(Input.create)
+    __call__ = staticmethod(TextFieldRoot.create)
 
 
 text_field = TextField()
--- reflex/testing.py
+++ reflex/testing.py
@@ -538,7 +538,7 @@ class AppHarness:
         if driver_clz is None:
             requested_driver = os.environ.get("APP_HARNESS_DRIVER", "Chrome")
             driver_clz = getattr(webdriver, requested_driver)
-            options = webdriver.ChromeOptions()
+            options = webdriver.FirefoxOptions()
         if driver_clz is webdriver.Chrome and want_headless:
             options = webdriver.ChromeOptions()
             options.add_argument("--headless=new")
