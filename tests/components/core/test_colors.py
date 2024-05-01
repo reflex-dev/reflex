5576850d0f5ec7727149c9f4e9b72cd657bb3fb9
khhan <khhan@umich.edu>
2024-05-01 14:43:37 -0400

    Changed import order in test_colors.py for ruff formatting

--- tests/components/core/test_colors.py
+++ tests/components/core/test_colors.py
@@ -1,8 +1,8 @@
 import pytest
 
 import reflex as rx
-from reflex.vars import Var
 from reflex.components.datadisplay.code import CodeBlock
+from reflex.vars import Var
 
 
 class ColorState(rx.State):
