12a35c7530baf4674a6c840da906622415c19158
Benedikt Bartscher <bb@senbax.de>
2024-05-03 20:48:54 +0200

    fix state names in test_var

--- tests/test_var.py
+++ tests/test_var.py
@@ -712,29 +712,26 @@ def test_var_unsupported_indexing_dicts(var, index):
 
 
 @pytest.mark.parametrize(
-    "fixture,full_name",
+    "fixture",
     [
-        ("ParentState", "parent_state.var_without_annotation"),
-        ("ChildState", "parent_state__child_state.var_without_annotation"),
-        (
-            "GrandChildState",
-            "parent_state__child_state__grand_child_state.var_without_annotation",
-        ),
-        ("StateWithAnyVar", "state_with_any_var.var_without_annotation"),
+        "ParentState",
+        "ChildState",
+        "GrandChildState",
+        "StateWithAnyVar",
     ],
 )
-def test_computed_var_without_annotation_error(request, fixture, full_name):
+def test_computed_var_without_annotation_error(request, fixture):
     """Test that a type error is thrown when an attribute of a computed var is
     accessed without annotating the computed var.
 
     Args:
         request: Fixture Request.
         fixture: The state fixture.
-        full_name: The full name of the state var.
     """
     with pytest.raises(TypeError) as err:
         state = request.getfixturevalue(fixture)
         state.var_without_annotation.foo
+    full_name = state.var_without_annotation._var_full_name
     assert (
         err.value.args[0]
         == f"You must provide an annotation for the state var `{full_name}`. Annotation cannot be `typing.Any`"
@@ -742,19 +739,13 @@ def test_computed_var_without_annotation_error(request, fixture, full_name):
 
 
 @pytest.mark.parametrize(
-    "fixture,full_name",
+    "fixture",
     [
-        (
-            "StateWithCorrectVarAnnotation",
-            "state_with_correct_var_annotation.var_with_annotation",
-        ),
-        (
-            "StateWithWrongVarAnnotation",
-            "state_with_wrong_var_annotation.var_with_annotation",
-        ),
+        "StateWithCorrectVarAnnotation",
+        "StateWithWrongVarAnnotation",
     ],
 )
-def test_computed_var_with_annotation_error(request, fixture, full_name):
+def test_computed_var_with_annotation_error(request, fixture):
     """Test that an Attribute error is thrown when a non-existent attribute of an annotated computed var is
     accessed or when the wrong annotation is provided to a computed var.
 
@@ -766,6 +757,7 @@ def test_computed_var_with_annotation_error(request, fixture, full_name):
     with pytest.raises(AttributeError) as err:
         state = request.getfixturevalue(fixture)
         state.var_with_annotation.foo
+    full_name = state.var_with_annotation._var_full_name
     assert (
         err.value.args[0]
         == f"The State var `{full_name}` has no attribute 'foo' or may have been annotated wrongly."
@@ -1375,12 +1367,15 @@ def test_invalid_var_operations(operand1_var: Var, operand2_var, operators: List
         (Var.create(1), "1"),
         (Var.create([1, 2, 3]), "[1, 2, 3]"),
         (Var.create({"foo": "bar"}), '{"foo": "bar"}'),
-        (Var.create(ATestState.value, _var_is_string=True), "a_test_state.value"),
+        (
+            Var.create(ATestState.value, _var_is_string=True),
+            f"{ATestState.get_full_name()}.value",
+        ),
         (
             Var.create(f"{ATestState.value} string", _var_is_string=True),
-            "`${a_test_state.value} string`",
+            f"`${{{ATestState.get_full_name()}.value}} string`",
         ),
-        (Var.create(ATestState.dict_val), "a_test_state.dict_val"),
+        (Var.create(ATestState.dict_val), f"{ATestState.get_full_name()}.dict_val"),
     ],
 )
 def test_var_name_unwrapped(var, expected):
