2ed3e70bae213d369691d9b7c4cd1a11fe0cdfde
Benedikt Bartscher <bb@senbax.de>
2024-05-08 21:44:07 +0200

    copy background task marker

--- reflex/state.py
+++ reflex/state.py
@@ -47,6 +47,7 @@ from redis.asyncio import Redis
 from reflex import constants
 from reflex.base import Base
 from reflex.event import (
+    BACKGROUND_TASK_MARKER,
     Event,
     EventHandler,
     EventSpec,
@@ -587,6 +588,8 @@ class BaseState(Base, ABC, extra=pydantic.Extra.allow):
             closure=fn.__closure__,
         )
         newfn.__annotations__ = fn.__annotations__
+        if mark := getattr(fn, BACKGROUND_TASK_MARKER, None):
+            setattr(newfn, BACKGROUND_TASK_MARKER, mark)
         return newfn
 
     @staticmethod
