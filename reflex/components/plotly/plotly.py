4084afa8602f907f1c332056d2d4ba6d81f895ed
Hongyu Yao <hongyuyao@hongyus-mbp-3.lan>
2024-06-03 10:11:06 -0700

    changed pyi

--- reflex/components/plotly/plotly.py
+++ reflex/components/plotly/plotly.py
@@ -109,8 +109,7 @@ class Plotly(PlotlyLib):
     config: Var[Dict]
 
     # If true, the graph will resize when the window is resized.
-    # use_resize_handler: Var[bool] = Var.create(True)
-    use_resize_handler: Var[bool]
+    use_resize_handler: Var[bool] = Var.create(True)
 
     # Fired after the plot is redrawn.
     on_after_plot: EventHandler[_passthrough_signature]
