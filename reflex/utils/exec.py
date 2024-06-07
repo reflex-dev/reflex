a53091fa87f7681bb17ec6d533642d99b6f3381b
Lendemor <thomas.brandeho@gmail.com>
2024-06-07 22:45:16 +0200

    update for nit

--- reflex/utils/exec.py
+++ reflex/utils/exec.py
@@ -108,13 +108,13 @@ def run_process_and_launch_url(run_command: list[str], backend_present=True):
                         url = match.group(1)
                         if get_config().frontend_path != "":
                             url = urljoin(url, get_config().frontend_path)
+
+                        console.print(
+                            f"App running at: [bold green]{url}[/bold green]{' (Frontend-only mode)' if not backend_present else ''}"
+                        )
                         if backend_present:
                             console.print(
-                                f"App running at: [bold green]{url}[/bold green] (Backend at: [bold green]{get_config().api_url})[/bold green]"
-                            )
-                        else:
-                            console.print(
-                                f"App running at: [bold green]{url}[/bold green] (Frontend-only mode)"
+                                f"Backend running at: [bold green]{get_config().api_url}[/bold green]"
                             )
                         first_run = False
                     else:
