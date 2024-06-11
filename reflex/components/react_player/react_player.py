1fb1201435fc88d6a4001c08f71a3644b9f305d7
Masen Furer <m_github@0x26.net>
2024-06-11 10:54:10 -0700

    Use new-style EventHandler props with comments
    Allow the PR to pass reflex-web integration test now that inline
    comments are respected for EventHandler props.

--- reflex/components/react_player/react_player.py
+++ reflex/components/react_player/react_player.py
@@ -2,9 +2,8 @@
 
 from __future__ import annotations
 
-from typing import Any, Dict
-
 from reflex.components.component import NoSSRComponent
+from reflex.event import EventHandler
 from reflex.vars import Var
 
 
@@ -46,27 +45,50 @@ class ReactPlayer(NoSSRComponent):
     # Set the height of the player: ex:640px
     height: Var[str]
 
-    def get_event_triggers(self) -> Dict[str, Any]:
-        """Get the event triggers.
+    # Called when media is loaded and ready to play. If playing is set to true, media will play immediately.
+    on_ready: EventHandler[lambda: []]
 
-        Returns:
-            The event triggers.
-        """
-        return {
-            "on_ready": lambda: [],
-            "on_start": lambda: [],
-            "on_play": lambda: [],
-            "on_progress": lambda e0: [e0],
-            "on_duration": lambda e0: [e0],
-            "on_pause": lambda: [],
-            "on_buffer": lambda: [],
-            "on_buffer_end": lambda: [],
-            "on_seek": lambda second: [second],
-            "on_playback_rate_change": lambda e0: [],
-            "on_playback_quality_change": lambda e0: [],
-            "on_ended": lambda: [],  # do not fire if loop=True
-            "on_error": lambda: [],
-            "on_click_preview": lambda: [],
-            "on_enable_pip": lambda: [],
-            "on_disable_pip": lambda: [],
-        }
+    # Called when media starts playing.
+    on_start: EventHandler[lambda: []]
+
+    # Called when media starts or resumes playing after pausing or buffering.
+    on_play: EventHandler[lambda: []]
+
+    # Callback containing played and loaded progress as a fraction, and playedSeconds and loadedSeconds in seconds. eg { played: 0.12, playedSeconds: 11.3, loaded: 0.34, loadedSeconds: 16.7 }
+    on_progress: EventHandler[lambda progress: [progress]]
+
+    # Callback containing duration of the media, in seconds.
+    on_duration: EventHandler[lambda seconds: [seconds]]
+
+    # Called when media is paused.
+    on_pause: EventHandler[lambda: []]
+
+    # Called when media starts buffering.
+    on_buffer: EventHandler[lambda: []]
+
+    # Called when media has finished buffering. Works for files, YouTube and Facebook.
+    on_buffer_end: EventHandler[lambda: []]
+
+    # Called when media seeks with seconds parameter.
+    on_seek: EventHandler[lambda seconds: [seconds]]
+
+    # Called when playback rate of the player changed. Only supported by YouTube, Vimeo (if enabled), Wistia, and file paths.
+    on_playback_rate_change: EventHandler[lambda e0: []]
+
+    # Called when playback quality of the player changed. Only supported by YouTube (if enabled).
+    on_playback_quality_change: EventHandler[lambda e0: []]
+
+    # Called when media finishes playing. Does not fire when loop is set to true.
+    on_ended: EventHandler[lambda: []]
+
+    # Called when an error occurs whilst attempting to play media.
+    on_error: EventHandler[lambda: []]
+
+    # Called when user clicks the light mode preview.
+    on_click_preview: EventHandler[lambda: []]
+
+    # Called when picture-in-picture mode is enabled.
+    on_enable_pip: EventHandler[lambda: []]
+
+    # Called when picture-in-picture mode is disabled.
+    on_disable_pip: EventHandler[lambda: []]
--- reflex/components/react_player/react_player.pyi
+++ reflex/components/react_player/react_player.pyi
@@ -7,12 +7,11 @@ from typing import Any, Dict, Literal, Optional, Union, overload
 from reflex.vars import Var, BaseVar, ComputedVar
 from reflex.event import EventChain, EventHandler, EventSpec
 from reflex.style import Style
-from typing import Any, Dict
 from reflex.components.component import NoSSRComponent
+from reflex.event import EventHandler
 from reflex.vars import Var
 
 class ReactPlayer(NoSSRComponent):
-    def get_event_triggers(self) -> Dict[str, Any]: ...
     @overload
     @classmethod
     def create(  # type: ignore
@@ -33,16 +32,28 @@ class ReactPlayer(NoSSRComponent):
         class_name: Optional[Any] = None,
         autofocus: Optional[bool] = None,
         custom_attrs: Optional[Dict[str, Union[Var, str]]] = None,
+        on_blur: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
         on_buffer: Optional[
             Union[EventHandler, EventSpec, list, function, BaseVar]
         ] = None,
         on_buffer_end: Optional[
             Union[EventHandler, EventSpec, list, function, BaseVar]
         ] = None,
+        on_click: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
         on_click_preview: Optional[
             Union[EventHandler, EventSpec, list, function, BaseVar]
         ] = None,
+        on_context_menu: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
         on_disable_pip: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
+        on_double_click: Optional[
             Union[EventHandler, EventSpec, list, function, BaseVar]
         ] = None,
         on_duration: Optional[
@@ -55,6 +66,33 @@ class ReactPlayer(NoSSRComponent):
             Union[EventHandler, EventSpec, list, function, BaseVar]
         ] = None,
         on_error: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
+        on_focus: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
+        on_mount: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
+        on_mouse_down: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
+        on_mouse_enter: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
+        on_mouse_leave: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
+        on_mouse_move: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
+        on_mouse_out: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
+        on_mouse_over: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
+        on_mouse_up: Optional[
             Union[EventHandler, EventSpec, list, function, BaseVar]
         ] = None,
         on_pause: Optional[
@@ -75,10 +113,16 @@ class ReactPlayer(NoSSRComponent):
         on_ready: Optional[
             Union[EventHandler, EventSpec, list, function, BaseVar]
         ] = None,
+        on_scroll: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
         on_seek: Optional[
             Union[EventHandler, EventSpec, list, function, BaseVar]
         ] = None,
         on_start: Optional[
+            Union[EventHandler, EventSpec, list, function, BaseVar]
+        ] = None,
+        on_unmount: Optional[
             Union[EventHandler, EventSpec, list, function, BaseVar]
         ] = None,
         **props
