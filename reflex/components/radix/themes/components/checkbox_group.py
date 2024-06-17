b78fa6f210bbc1bd4fe4e948b25fe6169d56e129
Emmanuel Onwuegbusi <34986076+emmakodes@users.noreply.github.com>
2024-06-17 22:02:20 +0100

    fix checkbox_group component (#3454)

--- reflex/components/radix/themes/components/checkbox_group.py
+++ reflex/components/radix/themes/components/checkbox_group.py
@@ -1,7 +1,7 @@
 """Components for the CheckboxGroup component of Radix Themes."""
 
 from types import SimpleNamespace
-from typing import Literal
+from typing import List, Literal
 
 from reflex.vars import Var
 
@@ -11,9 +11,9 @@ from ..base import LiteralAccentColor, RadixThemesComponent
 class CheckboxGroupRoot(RadixThemesComponent):
     """Root element for a CheckboxGroup component."""
 
-    tag = "CheckboxGroup"
+    tag = "CheckboxGroup.Root"
 
-    #
+    # Use the size prop to control the checkbox size.
     size: Var[Literal["1", "2", "3"]]
 
     # Variant of button: "classic" | "surface" | "soft"
@@ -25,11 +25,23 @@ class CheckboxGroupRoot(RadixThemesComponent):
     # Uses a higher contrast color for the component.
     high_contrast: Var[bool]
 
+    # determines which checkboxes, if any, are checked by default.
+    default_value: Var[List[str]]
+
+    # used to assign a name to the entire group of checkboxes
+    name: Var[str]
+
 
 class CheckboxGroupItem(RadixThemesComponent):
     """An item in the CheckboxGroup component."""
 
     tag = "CheckboxGroup.Item"
+
+    # specifies the value associated with a particular checkbox option.
+    value: Var[str]
+
+    # Use the native disabled attribute to create a disabled checkbox.
+    disabled: Var[bool]
 
 
 class CheckboxGroup(SimpleNamespace):
