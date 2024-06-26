ee747a429f7b1a36fdd8381989c8d841bcdb7fb7
Masen Furer <m_github@0x26.net>
2024-06-26 13:31:23 -0700

    run prettier on reflex/.templates/web/utils/helpers/color_mode.js

--- reflex/.templates/web/utils/helpers/color_mode.js
+++ reflex/.templates/web/utils/helpers/color_mode.js
@@ -1,12 +1,16 @@
-import {lastCompiledTimeStamp} from "/utils/context.js";
+import { lastCompiledTimeStamp } from "/utils/context.js";
 
-export function clearColorModeLocalStorageOnStartup () {
-    if (typeof window !== "undefined") {
-        const lastCompiledTimeInLocalStorage = localStorage.getItem("last_compiled_time")
-        if (lastCompiledTimeInLocalStorage && lastCompiledTimeInLocalStorage === lastCompiledTimeStamp) {
-            return
-        }
-        localStorage.removeItem('chakra-ui-color-mode');
-        localStorage.setItem("last_compiled_time", lastCompiledTimeStamp)
-      }
-};
+export function clearColorModeLocalStorageOnStartup() {
+  if (typeof window !== "undefined") {
+    const lastCompiledTimeInLocalStorage =
+      localStorage.getItem("last_compiled_time");
+    if (
+      lastCompiledTimeInLocalStorage &&
+      lastCompiledTimeInLocalStorage === lastCompiledTimeStamp
+    ) {
+      return;
+    }
+    localStorage.removeItem("chakra-ui-color-mode");
+    localStorage.setItem("last_compiled_time", lastCompiledTimeStamp);
+  }
+}
