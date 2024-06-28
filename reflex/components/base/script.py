5bc43823e1f2fb5bc6326f4eab137ee99d65c5a2
Masen Furer <m_github@0x26.net>
2024-06-28 11:16:14 -0700

    Default prop assignment must be Var to pass type checking

--- reflex/components/base/script.py
+++ reflex/components/base/script.py
@@ -31,7 +31,7 @@ class Script(Component):
 
     # When the script will execute: afterInteractive (defer-like behavior) | beforeInteractive | lazyOnload (async-like behavior)
     strategy: Var[Literal["afterInteractive", "beforeInteractive", "lazyOnload"]] = (
-        "afterInteractive"
+        Var.create_safe("afterInteractive", _var_is_string=True)
     )
 
     # Triggered when the script is loading
