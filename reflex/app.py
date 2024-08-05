6b1a73f9ef0916f0a275385449e8712f15144235
Benedikt Bartscher <bb@senbax.de>
2024-08-05 21:57:30 +0200

    Revert "move state.js to jinja, related to #3738"
    This reverts commit 4d2a72c4e86be3c8466cc701daf173ea70cba90d.

--- reflex/app.py
+++ reflex/app.py
@@ -943,8 +943,6 @@ class App(MiddlewareMixin, LifespanMixin, Base):
         compile_results.append(
             compiler.compile_contexts(self.state, self.theme),
         )
-        # Compile the state
-        compile_results.append(compiler.compile_state())
         # Fix #2992 by removing the top-level appearance prop
         if self.theme is not None:
             self.theme.appearance = None
