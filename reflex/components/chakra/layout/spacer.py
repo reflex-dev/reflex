020b1e3cc887f5890175ad4d0edab4a2ee464d2f
Elijah <elijahahianyo@gmail.com>
2024-08-15 12:17:20 +0000

    remove chakra components

--- reflex/components/chakra/layout/spacer.py
+++
@@ -1,9 +0,0 @@
-"""A flexible space component."""
-
-from reflex.components.chakra import ChakraComponent
-
-
-class Spacer(ChakraComponent):
-    """A flexible space component."""
-
-    tag = "Spacer"
