c457b43ab1e854d2b8e80be0a540409bedb01874
Masen Furer <m_github@0x26.net>
2024-08-30 16:36:33 -0700

    [REF-3416] [REF-3632] Update fastapi, gunicorn, watchdog deps (#3859)

--- reflex/utils/watch.py
+++ reflex/utils/watch.py
@@ -1,5 +1,7 @@
 """General utility functions."""
 
+from __future__ import annotations
+
 import contextlib
 import os
 import shutil
@@ -31,6 +33,22 @@ class AssetFolderWatch:
         self.observer.start()
 
 
+def _decode_path(path: str | bytes | bytearray | memoryview) -> str:
+    """Decode path to string.
+
+    Args:
+        path: The path to decode.
+
+    Returns:
+        The decoded path as a str.
+    """
+    if isinstance(path, (bytes, bytearray)):
+        return path.decode()
+    elif isinstance(path, memoryview):
+        return path.tobytes().decode()
+    return path
+
+
 class AssetFolderHandler(FileSystemEventHandler):
     """Asset folder event handler."""
 
@@ -51,19 +69,20 @@ class AssetFolderHandler(FileSystemEventHandler):
         Args:
             event: Event information.
         """
-        dest_path = self.get_dest_path(event.src_path)
+        src_path = _decode_path(event.src_path)
+        dest_path = self.get_dest_path(src_path)
 
         # wait 1 sec for fully saved
         time.sleep(1)
 
-        if os.path.isfile(event.src_path):
+        if os.path.isfile(src_path):
             with contextlib.suppress(PermissionError):
-                shutil.copyfile(event.src_path, dest_path)
-        if os.path.isdir(event.src_path):
+                shutil.copyfile(src_path, dest_path)
+        if os.path.isdir(src_path):
             if os.path.exists(dest_path):
                 shutil.rmtree(dest_path)
             with contextlib.suppress(PermissionError):
-                shutil.copytree(event.src_path, dest_path)
+                shutil.copytree(src_path, dest_path)
 
     def on_deleted(self, event: FileSystemEvent):
         """Event hander when a file or folder was deleted.
@@ -71,7 +90,7 @@ class AssetFolderHandler(FileSystemEventHandler):
         Args:
             event: Event infomation.
         """
-        dest_path = self.get_dest_path(event.src_path)
+        dest_path = self.get_dest_path(_decode_path(event.src_path))
 
         if os.path.isfile(dest_path):
             # when event is about a file, pass
