629850162ab3f7fadaf72ea94518270e232e9c66
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2024-08-30 17:26:10 -0700

    implement disk state manager (#3826)
    * implement disk state manager
    
    * put states inside of a folder
    
    * return root state all the time
    
    * factor out code
    
    * add docs for token expiration
    
    * cache states directory
    
    * call absolute on web directory
    
    * change dir to app path when testing the backend
    
    * remove accidental 🥒
    
    * test disk for now
    
    * modify schema
    
    * only serialize specific stuff
    
    * fix issue in types
    
    * what is a kilometer
    
    * create folder if it doesn't exist in write
    
    * this code hates me
    
    * check if the file isn't empty
    
    * add try except clause
    
    * add check for directory again

--- reflex/utils/path_ops.py
+++ reflex/utils/path_ops.py
@@ -81,6 +81,18 @@ def mkdir(path: str | Path):
     Path(path).mkdir(parents=True, exist_ok=True)
 
 
+def ls(path: str | Path) -> list[Path]:
+    """List the contents of a directory.
+
+    Args:
+        path: The path to the directory.
+
+    Returns:
+        A list of paths to the contents of the directory.
+    """
+    return list(Path(path).iterdir())
+
+
 def ln(src: str | Path, dest: str | Path, overwrite: bool = False) -> bool:
     """Create a symbolic link.
 
