11251d56a625dbff3e164e9963068885f75bd1b2
Elijah <elijahahianyo@gmail.com>
2024-09-04 21:13:45 +0000

    add warning when accessing `rx._x.progress`

--- reflex/experimental/__init__.py
+++ reflex/experimental/__init__.py
@@ -3,6 +3,7 @@
 from types import SimpleNamespace
 
 from reflex.components.props import PropsBase
+from reflex.components.radix.themes.components.progress import progress as progress
 from reflex.components.sonner.toast import toast as toast
 
 from ..utils.console import warn
@@ -31,10 +32,32 @@ class ExperimentalNamespace(SimpleNamespace):
         Returns:
             The toast namespace.
         """
-        if "toast" not in _EMITTED_PROMOTION_WARNINGS:
-            _EMITTED_PROMOTION_WARNINGS.add("toast")
-            warn(f"`rx._x.toast` was promoted to `rx.toast`.")
+        self.register_component_warning("toast")
         return toast
+
+    @property
+    def progress(self):
+        """Temporary property returning the toast namespace.
+
+        Remove this property when toast is fully promoted.
+
+        Returns:
+            The toast namespace.
+        """
+        self.register_component_warning("progress")
+        return progress
+
+    @staticmethod
+    def register_component_warning(component_name: str):
+        """Add component to emitted warnings and throw a warning if it
+        doesn't exist.
+
+        Args:
+             component_name: name of the component.
+        """
+        if component_name not in _EMITTED_PROMOTION_WARNINGS:
+            _EMITTED_PROMOTION_WARNINGS.add(component_name)
+            warn(f"`rx._x.{component_name}` was promoted to `rx.{component_name}`.")
 
 
 _x = ExperimentalNamespace(
