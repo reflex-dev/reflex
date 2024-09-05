dade940632a0c1533611b5c771b830cb01b14659
Elijah Ahianyo <elijahahianyo@gmail.com>
2024-09-05 17:21:32 +0000

    [REF-3592]Promote `rx.progress` from radix themes (#3878)
    * Promote `rx.progress` from radix themes
    
    * fix pyi
    
    * add warning when accessing `rx._x.progress`

--- reflex/components/radix/__init__.pyi
+++ reflex/components/radix/__init__.pyi
@@ -8,7 +8,6 @@ from . import themes as themes
 from .primitives.accordion import accordion as accordion
 from .primitives.drawer import drawer as drawer
 from .primitives.form import form as form
-from .primitives.progress import progress as progress
 from .themes.base import theme as theme
 from .themes.base import theme_panel as theme_panel
 from .themes.color_mode import color_mode as color_mode
@@ -31,6 +30,7 @@ from .themes.components.hover_card import hover_card as hover_card
 from .themes.components.icon_button import icon_button as icon_button
 from .themes.components.inset import inset as inset
 from .themes.components.popover import popover as popover
+from .themes.components.progress import progress as progress
 from .themes.components.radio_cards import radio_cards as radio_cards
 from .themes.components.radio_group import radio as radio
 from .themes.components.radio_group import radio_group as radio_group
