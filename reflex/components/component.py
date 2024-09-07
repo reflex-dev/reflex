0ad634b94f2f8f14d1b33abb01e95f21f62e38e1
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2024-09-06 17:09:22 -0700

    fix issue when on mount or on unmount is not set

--- reflex/components/component.py
+++ reflex/components/component.py
@@ -1352,15 +1352,15 @@ class Component(BaseComponent, ABC):
         on_mount = self.event_triggers.get(EventTriggers.ON_MOUNT, None)
         on_unmount = self.event_triggers.get(EventTriggers.ON_UNMOUNT, None)
         if on_mount is not None:
-            on_mount = str(LiteralVar.create(on_mount))
+            on_mount = str(LiteralVar.create(on_mount)) + "()"
         if on_unmount is not None:
-            on_unmount = str(LiteralVar.create(on_unmount))
+            on_unmount = str(LiteralVar.create(on_unmount)) + "()"
         if on_mount is not None or on_unmount is not None:
             return f"""
                 useEffect(() => {{
-                    ({on_mount or ""}())
+                    {on_mount or ""}
                     return () => {{
-                        ({on_unmount or ""}())
+                        {on_unmount or ""}
                     }}
                 }}, []);"""
 
