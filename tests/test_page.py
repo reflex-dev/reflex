f4658ae55c400e12a1a8c4b13e59ba5259f78b50
Lendemor <thomas.brandeho@gmail.com>
2024-09-18 13:17:52 -0700

    check content

--- tests/test_page.py
+++ tests/test_page.py
@@ -1,4 +1,5 @@
 from reflex import text
+from reflex.config import get_config
 from reflex.page import DECORATED_PAGES, page
 
 
@@ -10,6 +11,8 @@ def test_page_decorator():
     decorated_foo_ = page()(foo_)
     assert decorated_foo_ == foo_
     assert len(DECORATED_PAGES) == 1
+    page_data = DECORATED_PAGES.get(get_config().app_name, [])[0][1]
+    assert page_data == {}
     DECORATED_PAGES.clear()
 
 
@@ -17,6 +20,10 @@ def test_page_decorator_with_kwargs():
     def foo_():
         return text("foo")
 
+    def load_foo():
+        return []
+
+    DECORATED_PAGES.clear()
     assert len(DECORATED_PAGES) == 0
     decorated_foo_ = page(
         route="foo",
@@ -25,8 +32,19 @@ def test_page_decorator_with_kwargs():
         description="Foo description",
         meta=["foo-meta"],
         script_tags=["foo-script"],
-        on_load=lambda: [],
+        on_load=load_foo,
     )(foo_)
     assert decorated_foo_ == foo_
     assert len(DECORATED_PAGES) == 1
+    page_data = DECORATED_PAGES.get(get_config().app_name, [])[0][1]
+    assert page_data == {
+        "description": "Foo description",
+        "image": "foo.png",
+        "meta": ["foo-meta"],
+        "on_load": load_foo,
+        "route": "foo",
+        "script_tags": ["foo-script"],
+        "title": "Foo",
+    }
+
     DECORATED_PAGES.clear()
