b04dc00fda339cf96a96a1e2a1bcaebf61566429
Masen Furer <m_github@0x26.net>
2024-09-23 16:22:55 -0700

    Remove other references to python3.8

--- reflex/utils/prerequisites.py
+++ reflex/utils/prerequisites.py
@@ -304,7 +304,7 @@ def get_compiled_app(reload: bool = False, export: bool = False) -> ModuleType:
     """
     app_module = get_app(reload=reload)
     app = getattr(app_module, constants.CompileVars.APP)
-    # For py3.8 and py3.9 compatibility when redis is used, we MUST add any decorator pages
+    # For py3.9 compatibility when redis is used, we MUST add any decorator pages
     # before compiling the app in a thread to avoid event loop error (REF-2172).
     app._apply_decorated_pages()
     app._compile(export=export)
