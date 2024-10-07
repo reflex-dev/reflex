8013f3b4fe143a4158230ad2d4042cdce678295e
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2024-10-07 12:48:40 -0700

    upgrade default node to current lts

--- reflex/constants/installer.py
+++ reflex/constants/installer.py
@@ -79,7 +79,7 @@ class Node(SimpleNamespace):
     """Node/ NPM constants."""
 
     # The Node version.
-    VERSION = "18.17.0"
+    VERSION = "20.18.0"
     # The minimum required node version.
     MIN_VERSION = "18.17.0"
 
