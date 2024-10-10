4907b2e910ff5bfade5b8b1a915a4b0fad99b2b7
Carlos <36110765+carlosabadia@users.noreply.github.com>
2024-10-10 21:57:46 +0200

    default props comment for Grid (#4125)

--- reflex/components/recharts/cartesian.pyi
+++ reflex/components/recharts/cartesian.pyi
@@ -2047,10 +2047,10 @@ class Grid(Recharts):
 
         Args:
             *children: The children of the component.
-            x: The x-coordinate of grid.
-            y: The y-coordinate of grid.
-            width: The width of grid.
-            height: The height of grid.
+            x: The x-coordinate of grid. Default: 0
+            y: The y-coordinate of grid. Default: 0
+            width: The width of grid. Default: 0
+            height: The height of grid. Default: 0
             style: The style of the component.
             key: A unique key for the component.
             id: The id for the component.
@@ -2141,10 +2141,10 @@ class CartesianGrid(Grid):
             fill_opacity: The opacity of the background used to fill the space between grid lines.
             stroke_dasharray: The pattern of dashes and gaps used to paint the lines of the grid.
             stroke: the stroke color of grid. Default: rx.color("gray", 7)
-            x: The x-coordinate of grid.
-            y: The y-coordinate of grid.
-            width: The width of grid.
-            height: The height of grid.
+            x: The x-coordinate of grid. Default: 0
+            y: The y-coordinate of grid. Default: 0
+            width: The width of grid. Default: 0
+            height: The height of grid. Default: 0
             style: The style of the component.
             key: A unique key for the component.
             id: The id for the component.
@@ -2245,10 +2245,10 @@ class CartesianAxis(Grid):
             label: If set a string or a number, default label will be drawn, and the option is content.
             mirror: If set true, flips ticks around the axis line, displaying the labels inside the chart instead of outside. Default: False
             tick_margin: The margin between tick line and tick.
-            x: The x-coordinate of grid.
-            y: The y-coordinate of grid.
-            width: The width of grid.
-            height: The height of grid.
+            x: The x-coordinate of grid. Default: 0
+            y: The y-coordinate of grid. Default: 0
+            width: The width of grid. Default: 0
+            height: The height of grid. Default: 0
             style: The style of the component.
             key: A unique key for the component.
             id: The id for the component.
