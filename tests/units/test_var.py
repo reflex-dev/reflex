b1d449897a14d3c0646f08ed3b0809f314923544
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2024-10-11 17:27:15 -0700

    unionize base var fields types (#4153)
    * unionize base var fields types
    
    * add tests
    
    * fix union types for vars (#4152)
    
    * remove 3.11 special casing
    
    * special case on version
    
    * fix old versions of python
    
    ---------
    
    Co-authored-by: Masen Furer <m_github@0x26.net>

--- tests/units/test_var.py
+++ tests/units/test_var.py
@@ -1,5 +1,6 @@
 import json
 import math
+import sys
 import typing
 from typing import Dict, List, Optional, Set, Tuple, Union, cast
 
@@ -396,6 +397,44 @@ def test_list_tuple_contains(var, expected):
     other_var = Var(_js_expr="other", _var_type=str)
     assert str(var.contains(other_state_var)) == f"{expected}.includes(state.other)"
     assert str(var.contains(other_var)) == f"{expected}.includes(other)"
+
+
+class Foo(rx.Base):
+    """Foo class."""
+
+    bar: int
+    baz: str
+
+
+class Bar(rx.Base):
+    """Bar class."""
+
+    bar: str
+    baz: str
+    foo: int
+
+
+@pytest.mark.parametrize(
+    ("var", "var_type"),
+    (
+        [
+            (Var(_js_expr="", _var_type=Foo | Bar).guess_type(), Foo | Bar),
+            (Var(_js_expr="", _var_type=Foo | Bar).guess_type().bar, Union[int, str]),
+        ]
+        if sys.version_info >= (3, 10)
+        else []
+    )
+    + [
+        (Var(_js_expr="", _var_type=Union[Foo, Bar]).guess_type(), Union[Foo, Bar]),
+        (Var(_js_expr="", _var_type=Union[Foo, Bar]).guess_type().baz, str),
+        (
+            Var(_js_expr="", _var_type=Union[Foo, Bar]).guess_type().foo,
+            Union[int, None],
+        ),
+    ],
+)
+def test_var_types(var, var_type):
+    assert var._var_type == var_type
 
 
 @pytest.mark.parametrize(
