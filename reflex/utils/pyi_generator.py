e4eac2e3784ecd43832760e6a6139b809cc51494
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2024-10-11 17:08:40 -0700

    remove stray print
    Co-authored-by: Masen Furer <m_github@0x26.net>

--- reflex/utils/pyi_generator.py
+++ reflex/utils/pyi_generator.py
@@ -428,7 +428,6 @@ def _generate_component_create_functiondef(
     kwargs.extend(prop_kwargs)
 
     def figure_out_return_type(annotation: Any):
-        print(annotation)
         if inspect.isclass(annotation) and issubclass(annotation, inspect._empty):
             return ast.Name(id="Optional[EventType[[]]]")
         if isinstance(annotation, str) and annotation.startswith("Tuple["):
