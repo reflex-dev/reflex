1eacc659d7f23453be55808697249b6a5acde4bd
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2024-10-17 12:04:51 -0700

    noejs 22.10.0 is out

--- reflex/constants/installer.py
+++ reflex/constants/installer.py
@@ -80,7 +80,7 @@ class Node(SimpleNamespace):
     """Node/ NPM constants."""
 
     # The Node version.
-    VERSION = "22.9.0"
+    VERSION = "22.10.0"
     # The minimum required node version.
     MIN_VERSION = "18.17.0"
 
