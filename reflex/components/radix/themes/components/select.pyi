c310c6b080e5350c7d69bb32326ee3958b8a4297
Elijah <elijahahianyo@gmail.com>
2024-10-18 13:13:29 +0000

    pyi fix fr

--- reflex/components/radix/themes/components/select.pyi
+++ reflex/components/radix/themes/components/select.pyi
@@ -702,7 +702,6 @@ class HighLevelSelect(SelectRoot):
 
         Args:
             items: The items of the select.
-            items: The items of the select.
             placeholder: The placeholder of the select.
             label: The label of the select.
             color_scheme: The color of the select.
@@ -875,7 +874,6 @@ class Select(ComponentNamespace):
         """Create a select component.
 
         Args:
-            items: The items of the select.
             items: The items of the select.
             placeholder: The placeholder of the select.
             label: The label of the select.
