f39e8c96674a5e8bad5f42575a321c2af47c34ec
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2024-10-21 13:28:55 -0700

    move all environment variables to the same place (#4192)
    * move all environment variables to the same place
    
    * reorder things around
    
    * move more variables to environment
    
    * remove cyclical imports
    
    * forgot default value for field
    
    * for some reason type hints aren't being interpreted
    
    * put the field type *before* not after
    
    * make it get
    
    * move a bit more
    
    * add more fields
    
    * move reflex dir
    
    * add return
    
    * put things somewhere else
    
    * add custom error

--- reflex/utils/exceptions.py
+++ reflex/utils/exceptions.py
@@ -135,3 +135,7 @@ class SetUndefinedStateVarError(ReflexError, AttributeError):
 
 class StateSchemaMismatchError(ReflexError, TypeError):
     """Raised when the serialized schema of a state class does not match the current schema."""
+
+
+class EnvironmentVarValueError(ReflexError, ValueError):
+    """Raised when an environment variable is set to an invalid value."""
