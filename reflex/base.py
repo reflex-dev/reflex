9f655dbccd415f40198cf2711bdbb7f9aa47905f
Benedikt Bartscher <bb@senbax.de>
2024-10-28 21:36:43 +0100

    reload config is internal

--- reflex/base.py
+++ reflex/base.py
@@ -29,7 +29,7 @@ def validate_field_name(bases: List[Type["BaseModel"]], field_name: str) -> None
     from reflex.utils.exceptions import VarNameError
 
     # can't use reflex.config.environment here cause of circular import
-    reload = os.getenv("RELOAD_CONFIG", "").lower() == "true"
+    reload = os.getenv("__RELOAD_CONFIG", "").lower() == "true"
     for base in bases:
         try:
             if not reload and getattr(base, field_name, None):
