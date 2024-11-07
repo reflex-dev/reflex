387efeb39d28290c076a3b95dee7efb50623a3a6
Benedikt Bartscher <bb@senbax.de>
2024-11-07 20:39:45 +0100

    ruffing

--- tests/units/test_state_tree.py
+++ tests/units/test_state_tree.py
@@ -6,12 +6,7 @@ import pytest
 import pytest_asyncio
 
 import reflex as rx
-from reflex.state import (
-    BaseState,
-    StateManager,
-    StateManagerRedis,
-    _substate_key,
-)
+from reflex.state import BaseState, StateManager, StateManagerRedis, _substate_key
 
 
 class Root(BaseState):
