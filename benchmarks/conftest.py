3a3587fb7bb160ad91024b6d0a3cceb26cfede05
Benedikt Bartscher <bb@senbax.de>
2024-11-24 23:50:44 +0100

    benchmark compression and pickle protocols

--- benchmarks/conftest.py
+++ benchmarks/conftest.py
@@ -1,5 +1,7 @@
 """Shared conftest for all benchmark tests."""
 
+from typing import Tuple
+
 import pandas as pd
 import pytest
 
@@ -22,55 +24,41 @@ def app_harness_env(request):
     return request.param
 
 
-@pytest.fixture(params=[100000])
-def dict_size(request: pytest.FixtureRequest) -> int:
-    """The size of the dictionary.
-
-    Args:
-        request: The pytest fixture request object.
-
-    Returns:
-        The size of the dictionary.
-    """
-    return request.param
-
-
-@pytest.fixture(params=[100000])
-def df_size(request: pytest.FixtureRequest) -> int:
+@pytest.fixture(params=[(10, "SmallState"), (2000, "BigState")], ids=["small", "big"])
+def big_state_size(request: pytest.FixtureRequest) -> int:
     """The size of the DataFrame.
 
     Args:
         request: The pytest fixture request object.
 
     Returns:
-        The size of the DataFrame.
+        The size of the BigState
     """
     return request.param
 
 
 @pytest.fixture
-def big_state(
-    dict_size: int,
-    df_size: int,
-) -> State:
+def big_state(big_state_size: Tuple[int, str]) -> State:
     """A big state with a dictionary and a list of DataFrames.
 
     Args:
-        dict_size: The size of the dictionary.
-        df_size: The size of the DataFrame.
+        big_state_size: The size of the big state.
 
     Returns:
         A big state instance.
     """
+    size, _ = big_state_size
 
     class BigState(State):
         """A big state."""
 
         d: dict[str, int]
+        d_repeated: dict[str, int]
         df: list[pd.DataFrame]
 
-    d = {str(i): i for i in range(dict_size)}
-    df = [pd.DataFrame({"a": [i]}) for i in range(df_size)]
+    d = {str(i): i for i in range(size)}
+    d_repeated = {str(i): i for i in range(size)}
+    df = [pd.DataFrame({"a": [i]}) for i in range(size)]
 
-    state = BigState(d=d, df=df)
+    state = BigState(d=d, df=df, d_repeated=d_repeated)
     return state
