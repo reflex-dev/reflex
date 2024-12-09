9ff386bf488fb670e5438123ffb9db8b05b2b629
benedikt-bartscher <31854409+benedikt-bartscher@users.noreply.github.com>
2024-12-10 00:17:51 +0100

    add __repr__ method to MutableProxy (#4506)

--- reflex/state.py
+++ reflex/state.py
@@ -3599,6 +3599,14 @@ class MutableProxy(wrapt.ObjectProxy):
         self._self_state = state
         self._self_field_name = field_name
 
+    def __repr__(self) -> str:
+        """Get the representation of the wrapped object.
+
+        Returns:
+            The representation of the wrapped object.
+        """
+        return f"{self.__class__.__name__}({self.__wrapped__})"
+
     def _mark_dirty(
         self,
         wrapped=None,
