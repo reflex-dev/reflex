5cbdebb4eb65fefbaaecfec9dc4cfe3800674d5f
Elijah <elijahahianyo@gmail.com>
2024-12-18 15:20:34 +0000

    add app id as well

--- reflex/reflex.py
+++ reflex/reflex.py
@@ -442,6 +442,11 @@ def deploy(
         help="The name of the App to deploy under.",
         hidden=True,
     ),
+    app_id: str = typer.Option(
+        None,
+        "--app-id",
+        help="The ID of the App to deploy under.",
+    ),
     regions: List[str] = typer.Option(
         [],
         "-r",
@@ -529,6 +534,7 @@ def deploy(
     )
     hosting_cli.deploy(
         app_name=app_name,
+        app_id=app_id,
         export_fn=lambda zip_dest_dir,
         api_url,
         deploy_url,
