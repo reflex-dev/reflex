28568fd12f8c1e6717b8d506024f91b8b979bf46
Masen Furer <m_github@0x26.net>
2024-12-19 16:20:09 -0800

    Suppress exceptions from telemetry send (#4564)

--- reflex/reflex.py
+++ reflex/reflex.py
@@ -336,6 +336,7 @@ def login(loglevel: constants.LogLevel = typer.Option(config.loglevel)):
 
     validated_info = hosting_cli.login()
     if validated_info is not None:
+        _skip_compile()  # Allow running outside of an app dir
         telemetry.send("login", user_uuid=validated_info.get("user_id"))
 
 
