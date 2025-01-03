316a0c9bde7d3d03ed56709df01e94c74c83c44f
Masen Furer <m_github@0x26.net>
2025-01-03 15:50:01 -0800

    Do not allow call_function `callback` argument to be added afterwards (#4552)
    The default behavior for EventSpec is to treat the spec as a partial, wherein
    if additional unfilled arguments are available, and the event trigger wants to
    pass additional arguments, they will be applied positionally.
    
    However, it never makes sense to fill in `callback` with an event trigger arg.
    Therefore, if the callback is not provided initially, set it to None explicitly
    so that some invalid value cannot be added later.

--- reflex/event.py
+++ reflex/event.py
@@ -1190,7 +1190,7 @@ def call_function(
     Returns:
         EventSpec: An event that will execute the client side javascript.
     """
-    callback_kwargs = {}
+    callback_kwargs = {"callback": None}
     if callback is not None:
         callback_kwargs = {
             "callback": format.format_queue_events(
