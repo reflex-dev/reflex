6e546526b4c860d0e5955cb543bfdba4973aa9e3
Thomas Brandého <thomas.brandeho@gmail.com>
2025-01-16 12:49:54 -0800

    computed var default to cache=True (#4194)
    * computed var default to cache=True
    
    * fix lifespan integration tests
    
    * fix redis test

--- tests/units/test_var.py
+++ tests/units/test_var.py
@@ -1814,10 +1814,7 @@ def cv_fget(state: BaseState) -> int:
     ],
 )
 def test_computed_var_deps(deps: List[Union[str, Var]], expected: Set[str]):
-    @computed_var(
-        deps=deps,
-        cache=True,
-    )
+    @computed_var(deps=deps)
     def test_var(state) -> int:
         return 1
 
@@ -1835,10 +1832,7 @@ def test_computed_var_deps(deps: List[Union[str, Var]], expected: Set[str]):
 def test_invalid_computed_var_deps(deps: List):
     with pytest.raises(TypeError):
 
-        @computed_var(
-            deps=deps,
-            cache=True,
-        )
+        @computed_var(deps=deps)
         def test_var(state) -> int:
             return 1
 
