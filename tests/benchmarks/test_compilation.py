1b9671b6cc1d83e2a9c10ec362d57cfc96e74af9
Lendemor <thomas.brandeho@gmail.com>
2025-02-05 19:39:32 +0100

    add tests for _compile_stateful_components

--- tests/benchmarks/test_compilation.py
+++ tests/benchmarks/test_compilation.py
@@ -1,11 +1,16 @@
 import pytest
 
-from reflex.compiler.compiler import _compile_page
+from reflex.compiler.compiler import _compile_page, _compile_stateful_components
 
 
 @pytest.mark.benchmark
 def test_compile_page(evaluated_page):
     _compile_page(evaluated_page, None)
+
+
+@pytest.mark.benchmark
+def test_compile_stateful(evaluated_page):
+    _compile_stateful_components([evaluated_page])
 
 
 @pytest.mark.benchmark
