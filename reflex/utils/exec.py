3da4f6d3384e0596c2e840dadd583ad14819e779
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-02-10 16:20:57 -0800

    clear custom compoent cache between app harness tests

--- reflex/utils/exec.py
+++ reflex/utils/exec.py
@@ -253,15 +253,12 @@ def get_reload_dirs() -> list[Path]:
     if config.app_module is not None and config.app_module.__file__:
         module_path = Path(config.app_module.__file__).resolve().parent
 
-        while module_path.parent.name:
-            if any(
-                sibling_file.name == "__init__.py"
-                for sibling_file in module_path.parent.iterdir()
-            ):
-                # go up a level to find dir without `__init__.py`
-                module_path = module_path.parent
-            else:
-                break
+        while module_path.parent.name and any(
+            sibling_file.name == "__init__.py"
+            for sibling_file in module_path.parent.iterdir()
+        ):
+            # go up a level to find dir without `__init__.py`
+            module_path = module_path.parent
 
         reload_dirs = [module_path]
     return reload_dirs
