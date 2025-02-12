ce4360bc1d6bbdee60d663565100822cb87d9377
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-02-12 13:44:50 -0800

    benchmark experimentation

--- tests/benchmarks/test_compilation.py
+++ tests/benchmarks/test_compilation.py
@@ -1,18 +1,16 @@
-import pytest
+from pytest_codspeed import BenchmarkFixture
 
 from reflex.compiler.compiler import _compile_page, _compile_stateful_components
+from reflex.components.component import Component
 
 
-@pytest.mark.benchmark
-def test_compile_page(evaluated_page):
-    _compile_page(evaluated_page, None)
+def test_compile_page(evaluated_page: Component, benchmark: BenchmarkFixture):
+    benchmark(lambda: _compile_page(evaluated_page, None))
 
 
-@pytest.mark.benchmark
-def test_compile_stateful(evaluated_page):
-    _compile_stateful_components([evaluated_page])
+def test_compile_stateful(evaluated_page: Component, benchmark: BenchmarkFixture):
+    benchmark(lambda: _compile_stateful_components([evaluated_page]))
 
 
-@pytest.mark.benchmark
-def test_get_all_imports(evaluated_page):
-    evaluated_page._get_all_imports()
+def test_get_all_imports(evaluated_page: Component, benchmark: BenchmarkFixture):
+    benchmark(lambda: evaluated_page._get_all_imports())
