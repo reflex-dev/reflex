6e5efff201b0b3add2c1a484a1ffa4e03a3fe57b
Masen Furer <m_github@0x26.net>
2025-03-19 23:47:24 -0700

    remove next/link stuff from `rx.link`

--- reflex/components/radix/themes/typography/link.pyi
+++ reflex/components/radix/themes/typography/link.pyi
@@ -9,19 +9,15 @@ from reflex.components.component import MemoizationLeaf
 from reflex.components.core.breakpoints import Breakpoints
 from reflex.components.el.elements.inline import A
 from reflex.components.markdown.markdown import MarkdownComponentMap
-from reflex.components.next.link import NextLink
 from reflex.event import EventType
 from reflex.style import Style
-from reflex.utils.imports import ImportDict
 from reflex.vars.base import Var
 
 from ..base import RadixThemesComponent
 
 LiteralLinkUnderline = Literal["auto", "hover", "always", "none"]
-next_link = NextLink.create()
 
 class Link(RadixThemesComponent, A, MemoizationLeaf, MarkdownComponentMap):
-    def add_imports(self) -> ImportDict: ...
     @overload
     @classmethod
     def create(  # type: ignore
