1c908c21b1d4ae35d7c041e288b986822b5a6cb6
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-03-22 22:53:42 -0700

    change vite config to fix builds

--- reflex/.templates/web/vite.config.js
+++ reflex/.templates/web/vite.config.js
@@ -2,18 +2,25 @@ import { fileURLToPath, URL } from "url";
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig((config) => ({
   plugins: [reactRouter()],
   server: {
     port: process.env.PORT,
   },
   resolve: {
     alias: [
-      { find: "$", replacement: fileURLToPath(new URL("./", import.meta.url)) },
+      {
+        find: "$",
+        replacement: fileURLToPath(new URL("./", import.meta.url)),
+      },
       {
         find: "@",
         replacement: fileURLToPath(new URL("./public", import.meta.url)),
       },
-    ],
+    ].concat(
+      config.command === "build"
+        ? [{ find: "react-dom/server", replacement: "react-dom/server.node" }]
+        : []
+    ),
   },
-});
+}));
