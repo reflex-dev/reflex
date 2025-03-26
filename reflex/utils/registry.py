98993205fe193d12d234043573865c999aaf17bf
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-03-26 01:45:03 -0700

    the npm registry seems out of date

--- reflex/utils/registry.py
+++ reflex/utils/registry.py
@@ -48,11 +48,11 @@ def _get_best_registry() -> str:
     """
     console.debug("Getting best registry...")
     registries = [
-        "https://registry.npmjs.org",
-        "https://r.cnpmjs.org",
+        ("https://registry.npmjs.org", 1),
+        ("https://registry.npmmirror.com", 2),
     ]
 
-    best_registry = min(registries, key=average_latency)
+    best_registry = min(registries, key=lambda x: average_latency(x[0]) * x[1])[0]
     console.debug(f"Best registry: {best_registry}")
     return best_registry
 
