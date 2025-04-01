35bcbc5e52b266415d7a35d2ae5642e2b4d61044
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-04-01 16:44:24 -0700

    only compile once

--- hatch_build.py
+++ hatch_build.py
@@ -13,6 +13,17 @@ class CustomBuilder(BuildHookInterface):
 
     PLUGIN_NAME = "custom"
 
+    def marker(self) -> pathlib.Path:
+        """Get the marker file path.
+
+        Returns:
+            The marker file path.
+        """
+        return (
+            pathlib.Path(self.directory)
+            / f".reflex-{self.metadata.version}.pyi_generated"
+        )
+
     def finalize(
         self, version: str, build_data: dict[str, Any], artifact_path: str
     ) -> None:
@@ -23,6 +34,9 @@ class CustomBuilder(BuildHookInterface):
             build_data: The build data.
             artifact_path: The path to the artifact.
         """
+        if self.marker().exists():
+            return
+
         if not (pathlib.Path(self.root) / "scripts").exists():
             return
 
@@ -33,3 +47,4 @@ class CustomBuilder(BuildHookInterface):
             [sys.executable, "-m", "reflex.utils.pyi_generator"],
             check=True,
         )
+        self.marker().touch()
