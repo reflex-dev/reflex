002be2089494fc045438a37f9aded7ccebbc5f2d
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-04-02 16:07:09 -0700

    tf i'm doing

--- reflex/utils/telemetry.py
+++ reflex/utils/telemetry.py
@@ -203,6 +203,7 @@ def send_error(error: Exception, context: str):
     """
     if isinstance(error, ReflexError):
         # Don't send telemetry for ReflexError.
+        ...
         # return send("error", detail=type(error).__name__, context=context)
     else:
         return send("error", detail=type(error).__name__, context=context)
