665506f2c1fd3a444fa73215303228fa461cc30d
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-04-02 15:59:45 -0700

    maybe

--- reflex/utils/telemetry.py
+++ reflex/utils/telemetry.py
@@ -201,9 +201,4 @@ def send_error(error: Exception, context: str):
     Returns:
         Whether the telemetry was sent successfully.
     """
-    if isinstance(error, ReflexError):
-        return send("error", detail=type(error).__name__, context=context)
-    else:
-        print(
-            f"Error is not a ReflexError: {type(error).__name__}. Not sending telemetry."
-        )
+    return send("error", detail=type(error).__name__, context=context)
