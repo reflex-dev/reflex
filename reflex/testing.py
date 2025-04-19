3c8a0d4ac83a1dfbb2600b953d81383b9e3c7f42
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-04-19 04:10:36 -0700

    use export directly

--- reflex/testing.py
+++ reflex/testing.py
@@ -34,7 +34,7 @@ import reflex.utils.format
 import reflex.utils.prerequisites
 import reflex.utils.processes
 from reflex.components.component import CustomComponent
-from reflex.config import environment
+from reflex.config import environment, get_config
 from reflex.state import (
     BaseState,
     StateManager,
@@ -44,6 +44,7 @@ from reflex.state import (
     reload_state_module,
 )
 from reflex.utils import console
+from reflex.utils.export import export
 
 try:
     from selenium import webdriver
@@ -946,12 +947,18 @@ class AppHarnessProd(AppHarness):
             config.api_url = "http://{}:{}".format(
                 *self._poll_for_servers().getsockname(),
             )
-            reflex.reflex.export(
-                zip=True,
-                frontend_only=True,
-                backend_only=False,
-                loglevel=reflex.constants.LogLevel.INFO.value,
-                env=reflex.constants.Env.PROD.value,
+
+            if reflex.utils.prerequisites.needs_reinit(frontend=True):
+                reflex.reflex._init(
+                    name=get_config().app_name, loglevel=reflex.constants.LogLevel.INFO
+                )
+
+            export(
+                zipping=True,
+                frontend=True,
+                backend=False,
+                loglevel=reflex.constants.LogLevel.INFO,
+                env=reflex.constants.Env.PROD,
             )
 
         self.frontend_thread = threading.Thread(target=self._run_frontend)
