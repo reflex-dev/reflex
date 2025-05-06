7ac81e7cc8c8e2d48e207b8156564f61a4708136
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-05-06 15:51:26 -0700

    upgrade to nextjs 15.3.2

--- reflex/constants/installer.py
+++ reflex/constants/installer.py
@@ -75,7 +75,7 @@ fetch-retries=0
 
 
 def _determine_nextjs_version() -> str:
-    default_version = "15.3.1"
+    default_version = "15.3.2"
     if (version := os.getenv("NEXTJS_VERSION")) and version != default_version:
         from reflex.utils import console
 
