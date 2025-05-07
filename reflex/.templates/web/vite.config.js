4456aca829b1aca630ee03a9dec7ef1f529e16c3
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-05-06 18:43:09 -0700

    use unstable_optimizeDeps

--- reflex/.templates/web/vite.config.js
+++ reflex/.templates/web/vite.config.js
@@ -13,20 +13,6 @@ export default defineConfig((config) => ({
   server: {
     port: process.env.PORT,
   },
-  optimizeDeps: {
-    include: [
-      "@emotion/react",
-      "sonner",
-      "axios",
-      "socket.io-client",
-      "json5",
-      "universal-cookie",
-      "@radix-ui/themes",
-      "react-error-boundary",
-      "react-helmet",
-    ],
-    force: true,
-  },
   resolve: {
     alias: [
       {
