faf594f38928d836f7942099a77ae1242d6b422f
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-05-08 15:50:21 -0700

    use patch to precommit to use pyproject

--- scripts/hatch_build.py
+++ scripts/hatch_build.py
@@ -1,5 +1,6 @@
 """Custom build hook for Hatch."""
 
+import importlib.util
 import pathlib
 import subprocess
 import sys
@@ -36,6 +37,36 @@ class CustomBuilder(BuildHookInterface):
         """
         if self.marker().exists():
             return
+
+        if importlib.util.find_spec("pre_commit"):
+            import pre_commit.yaml
+            from diff_match_patch import diff_match_patch
+
+            patch = """@@ -82,16 +82,28 @@
+ ort yaml
++%0Aimport toml
+ %0A%0ALoader
+@@ -209,24 +209,28 @@
+ der=Loader)%0A
++def 
+ yaml_load = 
+@@ -226,37 +226,145 @@
+ aml_load
+- = functools.partial(
++(stream):%0A    try:%0A        return toml.loads(stream).get(%22tool%22, %7B%7D).get(%22pre-commit%22, %7B%7D)%0A    except ValueError:%0A        return 
+ yaml.loa
+@@ -364,16 +364,23 @@
+ aml.load
++(stream
+ , Loader
+"""  # noqa: W291
+
+            dmp = diff_match_patch()
+            patches = dmp.patch_fromText(patch)
+            new_text, _ = dmp.patch_apply(
+                patches, pathlib.Path(pre_commit.yaml.__file__).read_text()
+            )
+            pathlib.Path(pre_commit.yaml.__file__).write_text(new_text)
 
         if not (pathlib.Path(self.root) / "scripts").exists():
             return
