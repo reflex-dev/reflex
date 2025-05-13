4bdcbec1c47d8de0cc2db09c28bc174d674dff36
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-05-13 15:01:52 -0700

    precommit

--- reflex/components/lucide/icon.py
+++ reflex/components/lucide/icon.py
@@ -95,7 +95,9 @@ class DynamicIcon(LucideIconComponent):
         _imports = super()._get_imports()
         if self.library:
             _imports.pop(self.library)
-        _imports["lucide-react"] = [ImportVar("DynamicIcon", package_path="/dynamic.mjs")]
+        _imports["lucide-react"] = [
+            ImportVar("DynamicIcon", package_path="/dynamic.mjs")
+        ]
         return _imports
 
 
