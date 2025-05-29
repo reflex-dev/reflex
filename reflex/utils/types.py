12fc9f6552ffebaa1c81823f031c4fdaef37fd5a
Thomas Brandého <thomas.brandeho@gmail.com>
2025-05-29 04:32:41 +0200

    detect class var that are forward ref (#5362)
    * detect class var that are forward ref
    
    * cleanup

--- reflex/utils/types.py
+++ reflex/utils/types.py
@@ -263,8 +263,12 @@ def is_classvar(a_type: Any) -> bool:
     Returns:
         Whether the type is a ClassVar.
     """
-    return a_type is ClassVar or (
-        type(a_type) is _GenericAlias and a_type.__origin__ is ClassVar
+    return (
+        a_type is ClassVar
+        or (type(a_type) is _GenericAlias and a_type.__origin__ is ClassVar)
+        or (
+            type(a_type) is ForwardRef and a_type.__forward_arg__.startswith("ClassVar")
+        )
     )
 
 
