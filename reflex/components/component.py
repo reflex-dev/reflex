0e24ee13cc9f3289e24d654ee50fdc3a1fd27dc3
Lendemor <thomas.brandeho@gmail.com>
2025-05-30 15:20:32 -0700

    fix app_wraps not being passed from inside a rx.memo

--- reflex/components/component.py
+++ reflex/components/component.py
@@ -2206,6 +2206,15 @@ class CustomComponent(Component):
         component._add_style_recursive(style)
         return component
 
+    def _get_app_wrap_components(self) -> dict[tuple[int, str], Component]:
+        """Get the app wrap components for the custom component.
+
+        Returns:
+            The app wrap components.
+        """
+        # Custom components do not have app wrap components.
+        return self.get_component()._get_app_wrap_components()
+
 
 CUSTOM_COMPONENTS: dict[str, CustomComponent] = {}
 
