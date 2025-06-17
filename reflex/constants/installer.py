cd1a9e13c25fd33e16efb48ef1f9745e5ca4409b
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-06-17 16:42:32 -0700

    make run dev run react router dev instead of vite dev (#5460)

--- reflex/constants/installer.py
+++ reflex/constants/installer.py
@@ -104,7 +104,7 @@ class PackageJson(SimpleNamespace):
     class Commands(SimpleNamespace):
         """The commands to define in package.json."""
 
-        DEV = "vite dev"
+        DEV = "react-router dev"
         EXPORT = "react-router build"
         PROD = "serve ./build/client"
 
