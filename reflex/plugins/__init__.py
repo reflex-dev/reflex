cef489b425604abe5e7c9a93601965e5d41c8c0d
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-07-11 13:14:21 -0400

    enable sitemap plugin by default (#5558)
    * enable sitemap plugin by default
    
    * add prefix comment
    
    * make it a function on its own
    
    * adjust docstring for disable_plugins

--- reflex/plugins/__init__.py
+++ reflex/plugins/__init__.py
@@ -1,8 +1,15 @@
 """Reflex Plugin System."""
 
-from .base import CommonContext as CommonContext
-from .base import Plugin as Plugin
-from .base import PreCompileContext as PreCompileContext
-from .sitemap import Plugin as SitemapPlugin
-from .tailwind_v3 import TailwindV3Plugin as TailwindV3Plugin
-from .tailwind_v4 import TailwindV4Plugin as TailwindV4Plugin
+from .base import CommonContext, Plugin, PreCompileContext
+from .sitemap import SitemapPlugin
+from .tailwind_v3 import TailwindV3Plugin
+from .tailwind_v4 import TailwindV4Plugin
+
+__all__ = [
+    "CommonContext",
+    "Plugin",
+    "PreCompileContext",
+    "SitemapPlugin",
+    "TailwindV3Plugin",
+    "TailwindV4Plugin",
+]
--- reflex/plugins/sitemap.py
+++ reflex/plugins/sitemap.py
@@ -193,7 +193,7 @@ def sitemap_task(unevaluated_pages: Sequence["UnevaluatedPage"]) -> tuple[str, s
     )
 
 
-class Plugin(PluginBase):
+class SitemapPlugin(PluginBase):
     """Sitemap plugin for Reflex."""
 
     def pre_compile(self, **context):
@@ -204,3 +204,6 @@ class Plugin(PluginBase):
         """
         unevaluated_pages = context.get("unevaluated_pages", [])
         context["add_save_task"](sitemap_task, unevaluated_pages)
+
+
+Plugin = SitemapPlugin
