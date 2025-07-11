157651d9fa06df7f22dc28ea1face52d98ca5758
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-07-11 11:22:07 -0700

    check port with bind over connect_ex

--- reflex/utils/processes.py
+++ reflex/utils/processes.py
@@ -53,7 +53,7 @@ def get_num_workers() -> int:
     return (os.cpu_count() or 1) * 2 + 1
 
 
-def _is_address_responsive(
+def _can_bind_at_port(
     address_family: socket.AddressFamily | int, address: str, port: int
 ) -> bool:
     """Check if a given address and port are responsive.
@@ -68,9 +68,12 @@ def _is_address_responsive(
     """
     try:
         with closing(socket.socket(address_family, socket.SOCK_STREAM)) as sock:
-            return sock.connect_ex((address, port)) == 0
+            sock.bind((address, port))
+    except PermissionError:
+        return False
     except OSError:
         return False
+    return True
 
 
 def is_process_on_port(port: int) -> bool:
@@ -82,9 +85,9 @@ def is_process_on_port(port: int) -> bool:
     Returns:
         Whether a process is running on the given port.
     """
-    return _is_address_responsive(  # Test IPv4 localhost (127.0.0.1)
+    return not _can_bind_at_port(  # Test IPv4 localhost (127.0.0.1)
         socket.AF_INET, "127.0.0.1", port
-    ) or _is_address_responsive(
+    ) or not _can_bind_at_port(
         socket.AF_INET6, "::1", port
     )  # Test IPv6 localhost (::1)
 
