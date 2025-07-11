3c687f4c530d4b0abbc431ec86bbcd42e782fb57
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-07-11 13:22:52 -0400

    bump rolldown-vite and bun (#5563)

--- reflex/constants/installer.py
+++ reflex/constants/installer.py
@@ -14,7 +14,7 @@ class Bun(SimpleNamespace):
     """Bun constants."""
 
     # The Bun version.
-    VERSION = "1.2.17"
+    VERSION = "1.2.18"
 
     # Min Bun Version
     MIN_VERSION = "1.2.17"
@@ -143,7 +143,7 @@ class PackageJson(SimpleNamespace):
         "postcss-import": "16.1.1",
         "@react-router/dev": _react_router_version,
         "@react-router/fs-routes": _react_router_version,
-        "rolldown-vite": "7.0.5",
+        "rolldown-vite": "7.0.8",
     }
     OVERRIDES = {
         # This should always match the `react` version in DEPENDENCIES for recharts compatibility.
