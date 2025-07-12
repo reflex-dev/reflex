e20c257b8ce10656246c7370d1900dfea9aa7b01
Masen Furer <m_github@0x26.net>
2025-07-11 17:42:03 -0700

    human fixups
    * enable logging to stdout
    * allow prod mode to work
    * better port selection algorithm
    * remove useless code
    * cleaner teardown
    * test fixups for compatibility

--- tests/integration/test_input.py
+++ tests/integration/test_input.py
@@ -52,6 +52,11 @@ def FullyControlledInput():
             rx.button(
                 "CLEAR", on_click=rx.set_value("on_change_input", ""), id="clear"
             ),
+            rx.button(
+                "Clear Text Backend",
+                on_click=State.set_text(""),
+                id="clear-text-backend",
+            ),
         )
 
 
@@ -146,10 +151,8 @@ async def test_fully_controlled_input(fully_controlled_input: AppHarness):
     assert fully_controlled_input.poll_for_value(plain_value_input) == "ifoonitial"
 
     # clear the input on the backend
-    async with fully_controlled_input.modify_state(
-        f"{token}_{full_state_name}"
-    ) as state:
-        state.substates[state_name].text = ""
+    clear_text_backend = driver.find_element(By.ID, "clear-text-backend")
+    clear_text_backend.click()
     assert await get_state_text() == ""
     assert (
         fully_controlled_input.poll_for_value(
