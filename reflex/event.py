53237c540e20c621b0f46d98c353f5081e832ecd
Masen Furer <m_github@0x26.net>
2025-07-14 14:18:11 -0700

    ENG-6695: Do not allow call_script EventSpec to accept callback after being called

--- reflex/event.py
+++ reflex/event.py
@@ -1269,7 +1269,7 @@ def call_script(
     Returns:
         EventSpec: An event that will execute the client side javascript.
     """
-    callback_kwargs = {}
+    callback_kwargs = {"callback": None}
     if callback is not None:
         callback_kwargs = {
             "callback": str(
