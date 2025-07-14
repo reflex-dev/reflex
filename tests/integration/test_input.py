7d4bb925b8c6878185a2b3ed3e8b040256b34ac4
Masen Furer <m_github@0x26.net>
2025-07-14 09:50:22 -0700

    Fix test_input against timing race

--- tests/integration/test_input.py
+++ tests/integration/test_input.py
@@ -153,13 +153,13 @@ async def test_fully_controlled_input(fully_controlled_input: AppHarness):
     # clear the input on the backend
     clear_text_backend = driver.find_element(By.ID, "clear-text-backend")
     clear_text_backend.click()
-    assert await get_state_text() == ""
     assert (
         fully_controlled_input.poll_for_value(
             debounce_input, exp_not_equal="ifoonitial"
         )
         == ""
     )
+    assert await get_state_text() == ""
 
     # type more characters
     debounce_input.send_keys("getting testing done")
