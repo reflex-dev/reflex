acbe9d0745e8af29b5fb5eef5ce670664f9124df
Masen Furer <m_github@0x26.net>
2025-07-14 09:48:00 -0700

    Fix test_lifespan import error

--- tests/integration/test_lifespan.py
+++ tests/integration/test_lifespan.py
@@ -6,7 +6,6 @@ from collections.abc import Generator
 import httpx
 import pytest
 from selenium.webdriver.common.by import By
-from starlette.responses import JSONResponse
 
 from reflex.testing import AppHarness
 
@@ -24,6 +23,8 @@ def LifespanApp(
     """
     import asyncio
     from contextlib import asynccontextmanager
+
+    from starlette.responses import JSONResponse
 
     import reflex as rx
 
