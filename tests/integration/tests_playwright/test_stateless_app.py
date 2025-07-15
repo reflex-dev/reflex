45819bcb584172c6c52adcf90c401521bb2cd20b
Masen Furer <m_github@0x26.net>
2025-07-14 18:44:51 -0700

    fix missing rx import

--- tests/integration/tests_playwright/test_stateless_app.py
+++ tests/integration/tests_playwright/test_stateless_app.py
@@ -6,12 +6,12 @@ import httpx
 import pytest
 from playwright.sync_api import Page, expect
 
-import reflex as rx
 from reflex.testing import AppHarness
 
 
 def StatelessApp():
     """A stateless app that renders a heading."""
+    import reflex as rx
 
     def index():
         return rx.heading("This is a stateless app")
