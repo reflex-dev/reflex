8b3b2e986ad617c165e0cf36f62cba9078558a96
Masen Furer <m_github@0x26.net>
2025-07-14 18:19:00 -0700

    also ignore AccessDenied and keep trying

--- reflex/testing.py
+++ reflex/testing.py
@@ -378,7 +378,7 @@ class AppHarness:
                 raise RuntimeError(msg)
 
             for proc in process.children(recursive=True):
-                with contextlib.suppress(psutil.NoSuchProcess):
+                with contextlib.suppress(psutil.NoSuchProcess, psutil.AccessDenied):
                     if ncs := proc.net_connections():
                         for net_conn in ncs:
                             if net_conn.status == psutil.CONN_LISTEN:
