4ed4689ac70f0b240bb288206cb93c92baae051c
Maxim Vlah <m.vlah@senbax.de>
2025-08-05 13:05:01 +0200

    fix: closing in another loop?

--- reflex/app.py
+++ reflex/app.py
@@ -1926,6 +1926,11 @@ def upload(app: App):
                 )
             )
 
+        for file in files:
+            if not isinstance(file, StarletteUploadFile):
+                raise UploadValueError(
+                    "Uploaded file is not an UploadFile." + str(file)
+                )
             await file.close()
 
         event = Event(
