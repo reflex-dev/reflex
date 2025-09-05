910e0c75a76c489c929775bc36177addb53eeeae
benedikt-bartscher <31854409+benedikt-bartscher@users.noreply.github.com>
2025-09-05 20:12:15 +0200

    add sqlalchemy client pool configuration options via env vars (#5751)

--- reflex/environment.py
+++ reflex/environment.py
@@ -556,6 +556,18 @@ class EnvironmentVariables:
     # Whether to check db connections before using them.
     SQLALCHEMY_POOL_PRE_PING: EnvVar[bool] = env_var(True)
 
+    # The size of the database connection pool.
+    SQLALCHEMY_POOL_SIZE: EnvVar[int] = env_var(5)
+
+    # The maximum overflow size of the database connection pool.
+    SQLALCHEMY_MAX_OVERFLOW: EnvVar[int] = env_var(10)
+
+    # Recycle connections after this many seconds.
+    SQLALCHEMY_POOL_RECYCLE: EnvVar[int] = env_var(-1)
+
+    # The timeout for acquiring a connection from the pool.
+    SQLALCHEMY_POOL_TIMEOUT: EnvVar[int] = env_var(30)
+
     # Whether to ignore the redis config error. Some redis servers only allow out-of-band configuration.
     REFLEX_IGNORE_REDIS_CONFIG_ERROR: EnvVar[bool] = env_var(False)
 
