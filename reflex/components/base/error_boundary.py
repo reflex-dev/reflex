fe0f946dc0c240c6c1e513318c21db407e191c78
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-10-23 18:04:59 -0700

    make error boundary look nicer (#5926)
    * make error boundary look nicer
    
    * update tests

--- reflex/components/base/error_boundary.py
+++ reflex/components/base/error_boundary.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from reflex.components.component import Component
 from reflex.components.datadisplay.logo import svg_logo
-from reflex.components.el import a, button, details, div, h2, hr, p, pre, summary
+from reflex.components.el import a, button, div, h2, hr, p, pre, svg
 from reflex.event import EventHandler, set_clipboard
 from reflex.state import FrontendEventExceptionState
 from reflex.vars.base import Var
@@ -65,53 +65,67 @@ class ErrorBoundary(Component):
                     div(
                         div(
                             div(
+                                svg(
+                                    svg.circle(cx="12", cy="12", r="10"),
+                                    svg.path(d="M16 16s-1.5-2-4-2-4 2-4 2"),
+                                    svg.line(x1="9", x2="9.01", y1="9", y2="9"),
+                                    svg.line(x1="15", x2="15.01", y1="9", y2="9"),
+                                    xmlns="http://www.w3.org/2000/svg",
+                                    width="25vmin",
+                                    view_box="0 0 24 24",
+                                    class_name="lucide lucide-frown-icon lucide-frown",
+                                    custom_attrs={
+                                        "fill": "none",
+                                        "stroke": "currentColor",
+                                        "stroke-width": "2",
+                                        "stroke-linecap": "round",
+                                        "stroke-linejoin": "round",
+                                    },
+                                ),
                                 h2(
                                     "An error occurred while rendering this page.",
-                                    font_size="1.25rem",
+                                    font_size="5vmin",
                                     font_weight="bold",
                                 ),
-                                p(
-                                    "This is an error with the application itself.",
-                                    opacity="0.75",
-                                ),
-                                details(
-                                    summary("Error message", padding="0.5rem"),
-                                    div(
-                                        div(
-                                            pre(
-                                                Var(_js_expr=_ERROR_DISPLAY),
-                                            ),
-                                            padding="0.5rem",
-                                            width="fit-content",
-                                        ),
-                                        width="100%",
-                                        max_height="50vh",
-                                        overflow="auto",
-                                        background="#000",
-                                        color="#fff",
-                                        border_radius="0.25rem",
-                                    ),
-                                    button(
-                                        "Copy",
-                                        on_click=set_clipboard(
-                                            Var(_js_expr=_ERROR_DISPLAY)
-                                        ),
-                                        padding="0.35rem 0.75rem",
-                                        margin="0.5rem",
-                                        background="#fff",
-                                        color="#000",
-                                        border="1px solid #000",
-                                        border_radius="0.25rem",
-                                        font_weight="bold",
-                                    ),
-                                ),
+                                opacity="0.5",
                                 display="flex",
-                                flex_direction="column",
-                                gap="1rem",
-                                max_width="50ch",
-                                border="1px solid #888888",
-                                border_radius="0.25rem",
-                                padding="1rem",
+                                gap="4vmin",
+                                align_items="center",
+                            ),
+                            p(
+                                "This is an error with the application itself. Refreshing the page might help.",
+                                opacity="0.75",
+                                margin_block="1rem",
+                            ),
+                            div(
+                                div(
+                                    pre(
+                                        Var(_js_expr=_ERROR_DISPLAY),
+                                    ),
+                                    padding="0.5rem",
+                                    width="fit-content",
+                                ),
+                                width="100%",
+                                background="color-mix(in srgb, currentColor 5%, transparent)",
+                                max_height="15rem",
+                                overflow="auto",
+                                border_radius="0.4rem",
+                            ),
+                            button(
+                                "Copy",
+                                on_click=set_clipboard(Var(_js_expr=_ERROR_DISPLAY)),
+                                padding="0.35rem 1.35rem",
+                                margin_block="0.5rem",
+                                margin_inline_start="auto",
+                                background="color-mix(in srgb, currentColor 15%, transparent)",
+                                border_radius="0.4rem",
+                                width="fit-content",
+                                _hover={
+                                    "background": "color-mix(in srgb, currentColor 25%, transparent)"
+                                },
+                                _active={
+                                    "background": "color-mix(in srgb, currentColor 35%, transparent)"
+                                },
                             ),
                             hr(
                                 border_color="currentColor",
@@ -131,7 +145,10 @@ class ErrorBoundary(Component):
                             ),
                             display="flex",
                             flex_direction="column",
-                            gap="1rem",
+                            gap="0.5rem",
+                            max_width="min(80ch, 90vw)",
+                            border_radius="0.25rem",
+                            padding="1rem",
                         ),
                         height="100%",
                         width="100%",
