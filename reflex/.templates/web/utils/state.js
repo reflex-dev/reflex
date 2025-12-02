a78f358e39b9d7987bd851f26f280c2800cf019d
Masen Furer <m_github@0x26.net>
2025-12-02 14:36:20 -0800

    prettier 3.7.x update (#6017)
    https://prettier.io/blog/2025/11/27/3.7.0#change-18163

--- reflex/.templates/web/utils/state.js
+++ reflex/.templates/web/utils/state.js
@@ -56,10 +56,10 @@ export const generateUUID = () => {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
     let r = Math.random() * 16;
     if (d > 0) {
-      r = (d + r) % 16 | 0;
+      r = ((d + r) % 16) | 0;
       d = Math.floor(d / 16);
     } else {
-      r = (d2 + r) % 16 | 0;
+      r = ((d2 + r) % 16) | 0;
       d2 = Math.floor(d2 / 16);
     }
     return (c == "x" ? r : (r & 0x7) | 0x8).toString(16);
