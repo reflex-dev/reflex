b2c87a29e665b3381c0bb5ae8c16276300c1750c
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2026-01-21 11:25:31 -0800

    ok

--- reflex/.templates/web/utils/state.js
+++ reflex/.templates/web/utils/state.js
@@ -89,7 +89,7 @@ export const getToken = () => {
  * @returns The given URL modified to point to the actual backend server.
  */
 export const getBackendURL = (url_str) => {
-  if (url_str ?? undefined === undefined) {
+  if ((url_str ?? undefined) === undefined) {
     url_str = env.PING;
   }
   // Get backend URL object from the endpoint.
