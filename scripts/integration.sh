524cb740ec2c8fbbb1cb47343e2a96adbfd76307
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2026-03-25 11:29:32 -0700

    get back cleanup it to where it was

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -21,18 +21,7 @@ reflex run --loglevel debug --env "$env_mode" "$@" & pid=$!
 # Within the context of this bash, $pid_in_bash is what we need to pass to "kill" on exit
 # This is true on all platforms.
 pid_in_bash=$pid
-cleanup() {
-  kill -INT $pid_in_bash ||:
-  # Wait for the process to exit so it releases file locks (important on Windows)
-  for i in $(seq 1 10); do
-    kill -0 $pid_in_bash 2>/dev/null || return 0
-    sleep 1
-  done
-  # Force kill if still alive
-  kill -9 $pid_in_bash 2>/dev/null ||:
-  sleep 1
-}
-trap cleanup EXIT
+trap "kill -INT $pid_in_bash ||:" EXIT
 
 echo "Started server with PID $pid"
 
