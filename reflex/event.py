5b558aee3c81164339c911c0b620ed6655ff61e7
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2026-03-26 14:13:47 -0700

    fix import for event module

--- reflex/event.py
+++ reflex/event.py
@@ -1,3 +1,8 @@
 """Re-export from reflex_core."""
 
-from reflex_core.event import *
+import sys
+
+from reflex_core.event import *  # pyright: ignore[reportWildcardImportFromLibrary]
+from reflex_core.event import event
+
+sys.modules[__name__] = event  # pyright: ignore[reportArgumentType]
