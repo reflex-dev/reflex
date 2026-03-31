dc936c3acf264f4973dd13ee9fd485d30ed1f44c
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2026-03-31 12:03:31 -0700

    export modules for plugins (#6261)

--- reflex/plugins/__init__.py
+++ reflex/plugins/__init__.py
@@ -1,6 +1,5 @@
 """Re-export from reflex_core.plugins."""
 
-from reflex_core.plugins import *
 from reflex_core.plugins import (
     CommonContext,
     Plugin,
@@ -9,6 +8,9 @@ from reflex_core.plugins import (
     TailwindV3Plugin,
     TailwindV4Plugin,
     _ScreenshotPlugin,
+    sitemap,
+    tailwind_v3,
+    tailwind_v4,
 )
 
 __all__ = [
@@ -19,4 +21,7 @@ __all__ = [
     "TailwindV3Plugin",
     "TailwindV4Plugin",
     "_ScreenshotPlugin",
+    "sitemap",
+    "tailwind_v3",
+    "tailwind_v4",
 ]
