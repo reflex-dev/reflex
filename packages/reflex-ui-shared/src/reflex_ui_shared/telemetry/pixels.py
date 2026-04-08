327cd187feecb15f1c11f66e2ce878790e047819
Carlos <36110765+carlosabadia@users.noreply.github.com>
2026-04-08 11:07:25 -0700

    Add reflex-ui and integrations docs to packages (#6302)
    * Add reflex-ui and integrations docs to packages
    
    * update the imports
    
    * exclude docs
    
    * ruff

---
+++ packages/reflex-ui-shared/src/reflex_ui_shared/telemetry/pixels.py
@@ -0,0 +1,26 @@
+"""This module contains the pixel trackers for the website."""
+
+from reflex_ui.blocks.telemetry import (
+    get_default_telemetry_script,
+    get_google_analytics_trackers,
+    get_unify_trackers,
+    gtag_report_conversion,
+)
+
+import reflex as rx
+
+
+def get_pixel_website_trackers() -> list[rx.Component]:
+    """Get the pixel trackers for the website.
+
+    Returns:
+        The component.
+    """
+    return [
+        *get_google_analytics_trackers(tracking_id="G-4T7C8ZD9TR"),
+        gtag_report_conversion(
+            conversion_id_and_label="AW-11360851250/ASB4COvpisIbELKqo6kq"
+        ),
+        get_unify_trackers(),
+        get_default_telemetry_script(),
+    ]
---
+++ packages/reflex-ui/src/reflex_ui/components/base/textarea.py
@@ -0,0 +1,39 @@
+"""Custom Textarea component."""
+
+from reflex_components_core.el.elements.forms import Textarea as TextareaComponent
+
+from reflex.components.component import Component
+from reflex_ui.components.component import CoreComponent
+
+
+class ClassNames:
+    """Class names for textarea components."""
+
+    ROOT = "focus:shadow-[0px_0px_0px_2px_var(--primary-4)] focus:border-primary-7 focus:hover:border-primary-7 bg-secondary-1 border border-secondary-a4 hover:border-secondary-a6 transition-[color,box-shadow] disabled:border-secondary-4 disabled:bg-secondary-3 disabled:text-secondary-8 disabled:cursor-not-allowed cursor-text min-h-24 rounded-ui-md text-secondary-12 placeholder:text-secondary-9 text-sm disabled:placeholder:text-secondary-8 w-full outline-none max-h-[15rem] resize-none overflow-y-auto px-3 py-2.5 font-medium"
+
+
+class Textarea(TextareaComponent, CoreComponent):
+    """Root component for Textarea."""
+
+    @classmethod
+    def create(cls, *children, **props) -> Component:
+        """Create the textarea component.
+
+        Returns:
+            The component.
+        """
+        props.setdefault(
+            "custom_attrs",
+            {
+                "autoComplete": "off",
+                "autoCapitalize": "none",
+                "autoCorrect": "off",
+                "spellCheck": "false",
+            },
+        )
+        props["data-slot"] = "textarea"
+        cls.set_class_name(ClassNames.ROOT, props)
+        return super().create(*children, **props)
+
+
+textarea = Textarea.create
