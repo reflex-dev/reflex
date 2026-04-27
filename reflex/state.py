6903d40c2bdc5d9d8b7b65bd32b807f8275b6a06
Casey Riebe <casey@riebe.cloud>
2026-04-26 20:44:23 -0500

    Fixing local state var case

--- reflex/state.py
+++ reflex/state.py
@@ -1669,7 +1669,10 @@ class BaseState(EvenMoreBasicBaseState):
             raise UnretrievableVarValueError(msg)
         # Fastish case: this var belongs to this state
         if var_data.state == self.get_full_name():
-            return getattr(self, var_data.field_name)
+            value = getattr(self, var_data.field_name)
+            if inspect.isawaitable(value):
+                return await value
+            return value
 
         # Slow case: this var belongs to another state
         other_state = await self.get_state(
