96519f5a2ccd2785b09895dd0453f91f22fc43f5
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2026-06-16 13:40:19 -0700

    dev-build: apply biome formatting to postcss.config.js template

--- packages/reflex-base/src/reflex_base/.templates/web/postcss.config.js
+++ packages/reflex-base/src/reflex_base/.templates/web/postcss.config.js
@@ -1,4 +1,6 @@
-const noVendorPrefix = /^(1|true)$/i.test(process.env.REFLEX_NO_AUTOPREFIXER ?? "");
+const noVendorPrefix = /^(1|true)$/i.test(
+  process.env.REFLEX_NO_AUTOPREFIXER ?? "",
+);
 
 export default {
   plugins: {
