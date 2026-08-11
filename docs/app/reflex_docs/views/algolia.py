8612978ed314dd73bfa3dbe120bc94ee1dc83cef
Alek Petuskey <alekpetuskey@gmail.com>
2026-08-11 11:32:55 -0700

    Replace Inkeep with keyword-only Algolia search (#6859)
    * Replace Inkeep with keyword-only Algolia search
    
    * Fix search dialog layout in Safari
    
    * Add category icons to search results
    
    * docs: finish Algolia search implementation
    
    * docs: deprecate legacy Inkeep search aliases
    
    * fix mobile
    
    ---------
    
    Co-authored-by: carlosabadia <cutillascarlos@gmail.com>

---
+++ docs/app/reflex_docs/views/algolia.py
@@ -0,0 +1,5 @@
+"""Compatibility import for the shared Algolia search component."""
+
+from reflex_site_shared.components.algolia import AlgoliaSearch, Search, algolia_search
+
+__all__ = ["AlgoliaSearch", "Search", "algolia_search"]
