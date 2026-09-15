20f0c0b3c0c10d9ca719b02f325de1a2a1c20eb6
Alek <alek@reflex.dev>
2026-09-14 18:16:56 -0700

    Rename the Reflex Build sidebar section to Getting Started

--- docs/app/reflex_docs/templates/docpage/sidebar/sidebar.py
+++ docs/app/reflex_docs/templates/docpage/sidebar/sidebar.py
@@ -429,7 +429,7 @@ def sidebar_comp(
     )
     ai_builder_overview_content = rx.el.ul(
         create_sidebar_section(
-            "Overview",
+            "Getting Started",
             ai_builder_pages.overview.best_practices.path,
             ai_builder_overview_items,
             ai_builder_overview_index,
